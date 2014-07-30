e8e2d68601179d6fee103c49ac613048b50f2069
Alexander May <alexander.may@hjp-consulting.com>
2014-07-30 15:58:44 +0200

    Encode dynamic instance data in getAlgorithm instead of static dummys

--- de.persosim.simulator/src/de/persosim/simulator/crypto/DomainParameterSetEcdh.java
+++ de.persosim.simulator/src/de/persosim/simulator/crypto/DomainParameterSetEcdh.java
@@ -44,6 +44,7 @@ import de.persosim.simulator.utils.Utils;
 public class DomainParameterSetEcdh implements DomainParameterSet, TlvConstants {
 	
 	public static final byte[] id_ecPublicKey = HexString.toByteArray("2A8648CE3D0201");
+	public static final byte[] id_primeField = HexString.toByteArray("2A8648CE3D0101");
 	
 	//FIXME AMY correctly serialize DomainParameterSetEcdh
 //	@XmlElement
@@ -426,30 +427,31 @@ public class DomainParameterSetEcdh implements DomainParameterSet, TlvConstants 
 	@Override
 	public ConstructedTlvDataObject getAlgorithmIdentifier() {
 		
-		ConstructedTlvDataObject seq1 = new ConstructedTlvDataObject(TlvConstants.TAG_SEQUENCE);
-		seq1.addTlvDataObject(new PrimitiveTlvDataObject(TlvConstants.TAG_OID, HexString.toByteArray("2A8648CE3D0101")));
-		seq1.addTlvDataObject(new PrimitiveTlvDataObject(TlvConstants.TAG_INTEGER, HexString.toByteArray("00A9FB57DBA1EEA9BC3E660A909D838D726E3BF623D52620282013481D1F6E5377")));
-
-		ConstructedTlvDataObject seq2 = new ConstructedTlvDataObject(TlvConstants.TAG_SEQUENCE);
-		seq2.addTlvDataObject(new PrimitiveTlvDataObject(TlvConstants.TAG_OCTET_STRING, HexString.toByteArray("7D5A0975FC2C3057EEF67530417AFFE7FB8055C126DC5C6CE94A4B44F330B5D9")));
-		seq2.addTlvDataObject(new PrimitiveTlvDataObject(TlvConstants.TAG_OCTET_STRING, HexString.toByteArray("26DC5C6CE94A4B44F330B5D9BBD77CBF958416295CF7E1CE6BCCDC18FF8C07B6")));
+		PrimitiveTlvDataObject version = new PrimitiveTlvDataObject(TlvConstants.TAG_INTEGER, new byte[] {0x01});
 		
-		PrimitiveTlvDataObject os1 = new PrimitiveTlvDataObject(TlvConstants.TAG_OCTET_STRING, HexString.toByteArray("048BD2AEB9CB7E57CB2C4B482FFC81B7AFB9DE27E1E3BD23C23A4453BD9ACE3262547EF835C3DAC4FD97F8461A14611DC9C27745132DED8E545C1D54C72F046997"));
-		PrimitiveTlvDataObject int2 = new PrimitiveTlvDataObject(TlvConstants.TAG_INTEGER, HexString.toByteArray("00A9FB57DBA1EEA9BC3E660A909D838D718C397AA3B561A6F7901E0E82974856A7"));
-	   
+		ConstructedTlvDataObject fieldId = new ConstructedTlvDataObject(TlvConstants.TAG_SEQUENCE);
+		fieldId.addTlvDataObject(new PrimitiveTlvDataObject(TlvConstants.TAG_OID, id_primeField));
+		fieldId.addTlvDataObject(new PrimitiveTlvDataObject(TlvConstants.TAG_INTEGER, ((ECFieldFp) getCurve().getField()).getP().toByteArray()));
+				
+		ConstructedTlvDataObject curve = new ConstructedTlvDataObject(TlvConstants.TAG_SEQUENCE);
+		curve.addTlvDataObject(new PrimitiveTlvDataObject(TlvConstants.TAG_OCTET_STRING, getCurve().getA().toByteArray()));
+		curve.addTlvDataObject(new PrimitiveTlvDataObject(TlvConstants.TAG_OCTET_STRING, getCurve().getB().toByteArray()));
+		
+		PrimitiveTlvDataObject base = new PrimitiveTlvDataObject(TlvConstants.TAG_OCTET_STRING, CryptoUtil.encode(getGenerator(), getPublicPointReferenceLengthL(getPrime())));
+		PrimitiveTlvDataObject order = new PrimitiveTlvDataObject(TlvConstants.TAG_INTEGER, getOrder().toByteArray());
+		PrimitiveTlvDataObject cofactor = new PrimitiveTlvDataObject(TlvConstants.TAG_INTEGER, BigInteger.valueOf(getCofactor()).toByteArray());
+		
 		ConstructedTlvDataObject params = new ConstructedTlvDataObject(TlvConstants.TAG_SEQUENCE);
-		params.addTlvDataObject(new PrimitiveTlvDataObject(TlvConstants.TAG_INTEGER, new byte[] {0x01}));
-		params.addTlvDataObject(seq1);
-    	params.addTlvDataObject(seq2);
-    	params.addTlvDataObject(os1);
-    	params.addTlvDataObject(int2);
-    	params.addTlvDataObject(new PrimitiveTlvDataObject(TlvConstants.TAG_INTEGER, new byte[] {0x01}));
+		params.addTlvDataObject(version);
+		params.addTlvDataObject(fieldId);
+    	params.addTlvDataObject(curve);
+    	params.addTlvDataObject(base);
+    	params.addTlvDataObject(order);
+    	params.addTlvDataObject(cofactor);
 			    
 	    ConstructedTlvDataObject retVal = new ConstructedTlvDataObject(TlvConstants.TAG_SEQUENCE);
 		retVal.addTlvDataObject(new PrimitiveTlvDataObject(TlvConstants.TAG_OID, id_ecPublicKey));
 		retVal.addTlvDataObject(params);
-			    		
-		// FIXME AMY fill AlgorithmIdentifier with content
 		
 		return retVal;
 	}
