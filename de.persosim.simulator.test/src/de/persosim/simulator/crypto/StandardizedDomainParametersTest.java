2a3c6d46dab10aa9ad82021f22ad92ed39fb0dbd
Alexander May <alexander.may@hjp-consulting.com>
2014-07-30 17:17:06 +0200

    test simplifyAlgorithmIdentifier for all available domain parameters

--- de.persosim.simulator.test/src/de/persosim/simulator/crypto/StandardizedDomainParametersTest.java
+++ de.persosim.simulator.test/src/de/persosim/simulator/crypto/StandardizedDomainParametersTest.java
@@ -8,6 +8,7 @@ import de.persosim.simulator.test.PersoSimTestCase;
 import de.persosim.simulator.tlv.ConstructedTlvDataObject;
 import de.persosim.simulator.tlv.PrimitiveTlvDataObject;
 import de.persosim.simulator.tlv.TlvConstants;
+import de.persosim.simulator.utils.HexString;
 
 
 public class StandardizedDomainParametersTest extends PersoSimTestCase {
@@ -19,10 +20,12 @@ public class StandardizedDomainParametersTest extends PersoSimTestCase {
 	 */
 	@Test
 	public void testSimplifyAlgorithmIdentifier() {
-		//FIXME for(byte i = 0; i < StandardizedDomainParameters.NO_OF_STANDARDIZED_DOMAIN_PARAMETERS; i++) {
-		for(byte i = 13; i < 14; i++) {
+		for(byte i = 0; i < StandardizedDomainParameters.NO_OF_STANDARDIZED_DOMAIN_PARAMETERS; i++) {
 			DomainParameterSet domParams = StandardizedDomainParameters.getDomainParameterSetById(i);
 			if (domParams == null) continue;
+			
+			//dump configuration for includion in algIdentifierMapping
+			System.out.println(String.format("algIdentifierMapping.put(\"" + HexString.encode(domParams.getAlgorithmIdentifier().toByteArray()) + "\", 0x%02X);", i));
 			
 			ConstructedTlvDataObject expectedAlgIdentifier = new ConstructedTlvDataObject(TlvConstants.TAG_SEQUENCE);
 			expectedAlgIdentifier.addTlvDataObject(new PrimitiveTlvDataObject(TlvConstants.TAG_OID, StandardizedDomainParameters.OID));
