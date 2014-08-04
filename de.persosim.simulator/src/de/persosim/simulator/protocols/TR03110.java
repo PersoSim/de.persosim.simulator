4f8809c61a974cfe10d5cb2c72ee34ddca1e2881
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2014-08-04 09:59:49 +0200

    TR03110 added constants for password (PIN, CAN, ...) ids

--- de.persosim.simulator/src/de/persosim/simulator/protocols/TR03110.java
+++ de.persosim.simulator/src/de/persosim/simulator/protocols/TR03110.java
@@ -45,6 +45,9 @@ public class TR03110 implements TlvConstants {
 	public final static byte[] id_BSI                              = {0x04, 0x00, 0x7F, 0x00, 0x07};
 	public final static byte[] id_PK                               = Utils.appendBytes(id_BSI, new byte[]{0x02, 0x02, 0x01});
 	
+	public final static int ID_PIN = 3;
+	public final static int ID_CAN = 2;
+	
 	
 	
 	/**
