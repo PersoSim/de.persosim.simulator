f905debb6d38d95c61d61042a143ff8f80c73cd3
Martin Boonk <martin.boonk@hjp-consulting.com>
2014-08-06 10:26:59 +0200

    Added tests for the APDU rewrapping

--- de.persosim.simulator.test/src/de/persosim/simulator/apdu/InterindustryCommandApduTest.java
+++ de.persosim.simulator.test/src/de/persosim/simulator/apdu/InterindustryCommandApduTest.java
@@ -10,29 +10,29 @@ import org.junit.Test;
 import de.persosim.simulator.test.PersoSimTestCase;
 import de.persosim.simulator.utils.Utils;
 
-public class TR03110VerifySecureMessagingCommandApduTest extends PersoSimTestCase {
+public class InterindustryCommandApduTest extends PersoSimTestCase {
 	byte [] apduHeader;
 	byte [] commandData;
 	
 	@Before
 	public void setUp(){
-		apduHeader = new byte [] { (byte) 0x8C, 0x20, (byte) 0x80, 0x00};
+		apduHeader = new byte [] { (byte) 0x0C, 0x20, (byte) 0x80, 0x00};
 		commandData = new byte [] { 0x01, (byte) 0xFF };
 	}
 	
 	@Test
 	public void testGetSecureMessaging(){
-		IsoSecureMessagingCommandApdu apdu = new TR03110VerifySecureMessagingCommandApdu(Utils.concatByteArrays(apduHeader, commandData), null);
+		IsoSecureMessagingCommandApdu apdu = new InterindustryCommandApdu(Utils.concatByteArrays(apduHeader, commandData), null);
 		assertEquals(0b00000011, apdu.getSecureMessaging());
 	}
 
 	@Test
 	public void testRewrapApdu(){
-		IsoSecureMessagingCommandApdu apdu = new TR03110VerifySecureMessagingCommandApdu(Utils.concatByteArrays(apduHeader, commandData), null);
+		IsoSecureMessagingCommandApdu apdu = new InterindustryCommandApdu(Utils.concatByteArrays(apduHeader, commandData), null);
 		IsoSecureMessagingCommandApdu result = (IsoSecureMessagingCommandApdu) apdu.rewrapApdu((byte) 0, commandData);
 		
 		byte [] expectedHeader = Arrays.copyOf(apduHeader, apduHeader.length);
-		expectedHeader[0] = (byte) 0x80;
+		expectedHeader[0] = (byte) 0x00;
 		assertEquals(apdu.getCommandData(), result.getCommandData());
 		assertArrayEquals(expectedHeader, result.getHeader());
 	}
