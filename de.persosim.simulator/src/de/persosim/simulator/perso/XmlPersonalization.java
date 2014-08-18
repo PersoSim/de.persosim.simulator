42863e7faefbdd473dc645dac8b8f5dade96e5de
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2014-08-18 16:11:00 +0200

    XmlPersonalization: moved methods for retrieving MRZ and other passwords from here to GlobalTesterTest

--- de.persosim.simulator/src/de/persosim/simulator/perso/XmlPersonalization.java
+++ de.persosim.simulator/src/de/persosim/simulator/perso/XmlPersonalization.java
@@ -1,7 +1,6 @@
 package de.persosim.simulator.perso;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import javax.xml.bind.Unmarshaller;
@@ -11,13 +10,8 @@ import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import de.persosim.simulator.cardobjects.AuthObjectIdentifier;
-import de.persosim.simulator.cardobjects.CardObject;
 import de.persosim.simulator.cardobjects.MasterFile;
-import de.persosim.simulator.cardobjects.MrzAuthObject;
-import de.persosim.simulator.cardobjects.PasswordAuthObject;
 import de.persosim.simulator.protocols.Protocol;
-import de.persosim.simulator.protocols.Tr03110;
 
 @XmlRootElement(name="Personalization")
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -65,30 +59,6 @@ public class XmlPersonalization implements Personalization {
 				curPostProcessor.afterUnmarshall(this);	
 			}
 		}
-	}
-	
-	/**
-	 * This method returns the MRZ used for personalization.
-	 * @return the MRZ used for personalization
-	 */
-	protected String getMrz() {
-		Collection<CardObject> cardObjects = mf.findChildren(new AuthObjectIdentifier(Tr03110.ID_MRZ));
-		
-		MrzAuthObject mrzAuthObject = (MrzAuthObject) cardObjects.iterator().next();
-		
-		return mrzAuthObject.getMrz();
-	}
-	
-	/**
-	 * This method returns the requested password as set during personalization.
-	 * Valid password identifiers as set in {@link Tr03110} e.g. are ID_MRZ, ID_CA, ID_PIN, ID_PUK.
-	 * @param passwordIdentifier the password identifier
-	 * @return the requested password as set during personalization
-	 */
-	protected byte[] getPassword(int passwordIdentifier) {
-		Collection<CardObject> cardObjects = mf.findChildren(new AuthObjectIdentifier(passwordIdentifier));
-		PasswordAuthObject pwdAuthObject = (PasswordAuthObject) cardObjects.iterator().next();
-		return pwdAuthObject.getPassword();
 	}
 	
 }
