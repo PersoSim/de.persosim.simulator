07089336bad84070befac90f5e84af45a289d819
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2014-08-19 09:57:35 +0200

    added tests for new personalization samples performing PACE

---
+++ de.persosim.simulator.test/gtTests/de/persosim/simulator/test/globaltester/perso/TestPerso001Test.java
@@ -0,0 +1,33 @@
+package de.persosim.simulator.test.globaltester.perso;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import de.persosim.simulator.perso.DefaultPersoTestPkiTemplate01;
+import de.persosim.simulator.perso.Personalization;
+import de.persosim.simulator.test.globaltester.GtConstants;
+import de.persosim.simulator.test.globaltester.GtSuiteDescriptor;
+import de.persosim.simulator.test.globaltester.JobDescriptor;
+
+public class TestPerso001Test extends GtDefaultPersoTest {
+
+	@Override
+	public Personalization getPersonalization() {
+		if(persoCache == null) {
+			persoCache = new DefaultPersoTestPkiTemplate01();
+		}
+		
+		return persoCache;
+	}
+	
+	@Override
+	public Collection<JobDescriptor> getAllApplicableGtTests() {
+		Collection<JobDescriptor> retVal = 
+		new ArrayList<JobDescriptor>();
+		
+		retVal.add(new GtSuiteDescriptor(GtConstants.PROJECT_EPA_EAC2_BSI, "EAC2_ISO7816_H_04a"));
+
+		return retVal;
+	}
+	
+}
