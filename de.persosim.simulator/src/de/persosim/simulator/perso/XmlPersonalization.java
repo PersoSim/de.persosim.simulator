0c03ad1415a434093f842b81282c11ebd61d908b
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2014-08-19 08:35:10 +0200

    XmlPersonalization: added writeToFile method

--- de.persosim.simulator/src/de/persosim/simulator/perso/XmlPersonalization.java
+++ de.persosim.simulator/src/de/persosim/simulator/perso/XmlPersonalization.java
@@ -1,8 +1,12 @@
 package de.persosim.simulator.perso;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -11,6 +15,7 @@ import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import de.persosim.simulator.cardobjects.MasterFile;
+import de.persosim.simulator.jaxb.PersoSimJaxbContextProvider;
 import de.persosim.simulator.protocols.Protocol;
 
 @XmlRootElement(name="Personalization")
@@ -58,6 +63,28 @@ public class XmlPersonalization implements Personalization {
 			for (PersoUnmarshallerCallback curPostProcessor : unmarshallerCallbacks) {
 				curPostProcessor.afterUnmarshall(this);	
 			}
+		}
+	}
+	
+	/**
+	 * This method writes a personalization to a file identified by a provided file name.
+	 * @param fileName the file name to use
+	 */
+	public void writeToFile(String fileName) {
+		try {
+			// instantiate marshaller
+			Marshaller m = PersoSimJaxbContextProvider.getContext().createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+					
+			// Write to File
+			File xmlFile = new File(fileName);
+			m.marshal(this, xmlFile);
+		} catch (PropertyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
