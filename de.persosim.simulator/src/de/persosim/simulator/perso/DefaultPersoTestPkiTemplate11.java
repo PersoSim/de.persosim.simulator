8622e8b008926b4211518fff0751cd32d4ab69a8
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2014-09-02 13:46:40 +0200

    Updated personalization templates to match recent generalizing changes in super class

--- de.persosim.simulator/src/de/persosim/simulator/perso/DefaultPersoTestPkiTemplate11.java
+++ de.persosim.simulator/src/de/persosim/simulator/perso/DefaultPersoTestPkiTemplate11.java
@@ -1,33 +1,8 @@
 package de.persosim.simulator.perso;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.Date;
 
-import de.persosim.simulator.cardobjects.AuthObjectIdentifier;
-import de.persosim.simulator.cardobjects.ByteDataAuxObject;
-import de.persosim.simulator.cardobjects.CardFile;
-import de.persosim.simulator.cardobjects.ChangeablePasswordAuthObject;
-import de.persosim.simulator.cardobjects.DateAuxObject;
-import de.persosim.simulator.cardobjects.DedicatedFile;
-import de.persosim.simulator.cardobjects.ElementaryFile;
-import de.persosim.simulator.cardobjects.FileIdentifier;
-import de.persosim.simulator.cardobjects.Iso7816LifeCycleState;
-import de.persosim.simulator.cardobjects.MrzAuthObject;
-import de.persosim.simulator.cardobjects.OidIdentifier;
-import de.persosim.simulator.cardobjects.PasswordAuthObject;
-import de.persosim.simulator.cardobjects.PasswordAuthObjectWithRetryCounter;
-import de.persosim.simulator.cardobjects.PinObject;
-import de.persosim.simulator.cardobjects.ShortFileIdentifier;
-import de.persosim.simulator.protocols.ta.TaOid;
-import de.persosim.simulator.secstatus.PaceSecurityCondition;
-import de.persosim.simulator.secstatus.SecCondition;
-import de.persosim.simulator.utils.HexString;
+import de.persosim.simulator.tlv.ConstructedTlvDataObject;
 
 /**
  * @author slutters
@@ -36,192 +11,58 @@ import de.persosim.simulator.utils.HexString;
 public class DefaultPersoTestPkiTemplate11 extends DefaultPersoTestPkiTemplate {
 	
 	@Override
-	protected void addAuxData() {
-		// Aux data
-		byte[] communityId = HexString.toByteArray("02760509130000"); // updated
+	public String getEidDg4PlainData() {
+		return "HILDEGARD";
+	}
+
+	@Override
+	public String getEidDg5PlainData() {
+		return "MÜLLER";
+	}
+
+	@Override
+	public String getEidDg8PlainData() {
+		return "19390204";
+	}
+	
+	@Override
+	public String getEidDg9PlainData() {
+		return "SAARBRÜCKEN";
+	}
+
+	@Override
+	public String getEidDg11PlainData() {
+		return "F";
+	}
+	
+	@Override
+	public byte[] getEidDg17Data() {
+		ConstructedTlvDataObject dg17Tlv; 
 		
-		Calendar calendar = Calendar.getInstance();
+		try {
+			dg17Tlv = DefaultPersoTestPkiTemplate.createEidDg17Tlv("HARKORTSTR. 58", "DORTMUND", null, "D", "44225");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			dg17Tlv = null;
+		}
 		
-		calendar.set(1939, Calendar.FEBRUARY, 4, 0, 0, 0); // updated
-		calendar.set(Calendar.MILLISECOND, 0);
-		Date dateOfBirth = calendar.getTime();
-		
-		calendar.set(2010, Calendar.DECEMBER, 31, 0, 0, 0); // updated
-		calendar.set(Calendar.MILLISECOND, 0);
-		Date validityDate = calendar.getTime();
-
-		mf.addChild(new ByteDataAuxObject(new OidIdentifier(
-				TaOid.id_CommunityID), communityId));
-		mf.addChild(new DateAuxObject(new OidIdentifier(TaOid.id_DateOfBirth),
-				dateOfBirth));
-		mf.addChild(new DateAuxObject(new OidIdentifier(TaOid.id_DateOfExpiry),
-				validityDate));
-	}
-	
-	@Override
-	protected void addEidDg3(DedicatedFile eIdAppl) {
-		CardFile eidDg3 = new ElementaryFile(new FileIdentifier(0x0103),
-				new ShortFileIdentifier(0x03),
-				HexString.toByteArray("6306120420101231"),
-				getAccessRightReadEidDg(3),
-				Collections.<SecCondition> emptySet(),
-				Collections.<SecCondition> emptySet());
-		eIdAppl.addChild(eidDg3);
-	}
-	
-	@Override
-	protected void addEidDg4(DedicatedFile eIdAppl) {
-		CardFile eidDg4 = new ElementaryFile(new FileIdentifier(0x0104),
-				new ShortFileIdentifier(0x04),
-				HexString.toByteArray("640B120948494C444547415244"),
-				getAccessRightReadEidDg(4),
-				Collections.<SecCondition> emptySet(),
-				Collections.<SecCondition> emptySet());
-		eIdAppl.addChild(eidDg4);
-	}
-	
-	@Override
-	protected void addEidDg5(DedicatedFile eIdAppl) {
-		CardFile eidDg5 = new ElementaryFile(
-				new FileIdentifier(0x0105),
-				new ShortFileIdentifier(0x05),
-				HexString
-						.toByteArray("65090C074DC39C4C4C4552"),
-				getAccessRightReadEidDg(5), Collections
-						.<SecCondition> emptySet(), Collections
-						.<SecCondition> emptySet());
-		eIdAppl.addChild(eidDg5);
-	}
-	
-	@Override
-	protected void addEidDg6(DedicatedFile eIdAppl) {
-		CardFile eidDg6 = new ElementaryFile(
-				new FileIdentifier(0x0106),
-				new ShortFileIdentifier(0x06),
-				HexString
-						.toByteArray("66020C00"),
-				getAccessRightReadEidDg(6), Collections
-						.<SecCondition> emptySet(), Collections
-						.<SecCondition> emptySet());
-		eIdAppl.addChild(eidDg6);
-	}
-	
-	@Override
-	protected void addEidDg7(DedicatedFile eIdAppl) {
-		CardFile eidDg7 = new ElementaryFile(new FileIdentifier(0x0107),
-				new ShortFileIdentifier(0x07),
-				HexString.toByteArray("67020C00"),
-				getAccessRightReadEidDg(7),
-				Collections.<SecCondition> emptySet(),
-				Collections.<SecCondition> emptySet());
-		eIdAppl.addChild(eidDg7);
-	}
-	
-	@Override
-	protected void addEidDg8(DedicatedFile eIdAppl) {
-		CardFile eidDg8 = new ElementaryFile(new FileIdentifier(0x0108),
-				new ShortFileIdentifier(0x08),
-				HexString.toByteArray("6806120419390204"),
-				getAccessRightReadEidDg(8),
-				Collections.<SecCondition> emptySet(),
-				Collections.<SecCondition> emptySet());
-		eIdAppl.addChild(eidDg8);
-	}
-	
-	@Override
-	protected void addEidDg9(DedicatedFile eIdAppl) {
-		CardFile eidDg9 = new ElementaryFile(
-				new FileIdentifier(0x0109),
-				new ShortFileIdentifier(0x09),
-				HexString
-						.toByteArray("6910A10E0C0C534141524252C39C434B454E"),
-				getAccessRightReadEidDg(9), Collections
-						.<SecCondition> emptySet(), Collections
-						.<SecCondition> emptySet());
-		eIdAppl.addChild(eidDg9);
-	}
-	
-	@Override
-	protected void addEidDg11(DedicatedFile eIdAppl) {
-		CardFile eidDg11 = new ElementaryFile(new FileIdentifier(0x010B),
-				new ShortFileIdentifier(0x0B),
-				HexString.toByteArray("6B03130146"),
-				getAccessRightReadEidDg(11),
-				Collections.<SecCondition> emptySet(),
-				Collections.<SecCondition> emptySet());
-		eIdAppl.addChild(eidDg11);
-	}
-	
-	@Override
-	protected void addEidDg13(DedicatedFile eIdAppl) {
-		CardFile eidDg13 = new ElementaryFile(new FileIdentifier(0x010D),
-				new ShortFileIdentifier(0x0D),
-				HexString.toByteArray("6D020C00"),
-				getAccessRightReadEidDg(13),
-				Collections.<SecCondition> emptySet(),
-				Collections.<SecCondition> emptySet());
-		eIdAppl.addChild(eidDg13);
-	}
-	
-	@Override
-	protected void addEidDg17(DedicatedFile eIdAppl) {
-		CardFile eidDg17 = new ElementaryFile(
-				new FileIdentifier(0x0111),
-				new ShortFileIdentifier(0x11),
-				HexString
-						.toByteArray("712E302CAA100C0E4841524B4F52545354522E203538AB0A0C08444F52544D554E44AD03130144AE0713053434323235"),
-				getAccessRightReadEidDg(17), getAccessRightUpdateEidDg(17),
-				Collections.<SecCondition> emptySet());
-		eIdAppl.addChild(eidDg17);
-	}
-	
-	@Override
-	protected void addEidDg18(DedicatedFile eIdAppl) {
-		CardFile eidDg18 = new ElementaryFile(new FileIdentifier(0x0112),
-				new ShortFileIdentifier(0x12),
-				HexString.toByteArray("7209040702760509130000"),
-				getAccessRightReadEidDg(18), getAccessRightUpdateEidDg(18),
-				Collections.<SecCondition> emptySet());
-		eIdAppl.addChild(eidDg18);
+		return dg17Tlv.toByteArray();
 	}
 
 	@Override
-	protected void addAuthObjects() throws NoSuchAlgorithmException,
-			NoSuchProviderException, IOException, UnsupportedEncodingException {
-		MrzAuthObject mrz = new MrzAuthObject(
-				new AuthObjectIdentifier(1),
-				"IDD<<0123456784<<<<<<<<<<<<<<<3902046F1012312D<<<<<<<<<<<<<4MUELLER<<HILDEGARD<<<<<<<<<<<<");
-		mf.addChild(mrz);
-
-		ChangeablePasswordAuthObject can = new ChangeablePasswordAuthObject(
-				new AuthObjectIdentifier(2), "500540".getBytes("UTF-8"), "CAN",
-				6, 6);
-		can.updateLifeCycleState(Iso7816LifeCycleState.OPERATIONAL_ACTIVATED);
-		mf.addChild(can);
-
-		PasswordAuthObjectWithRetryCounter pin = new PinObject(
-				new AuthObjectIdentifier(3), "123456".getBytes("UTF-8"), 6, 6,
-				3);
-		pin.updateLifeCycleState(Iso7816LifeCycleState.OPERATIONAL_ACTIVATED);
-		mf.addChild(pin);
-
-		PasswordAuthObject puk = new PasswordAuthObject(
-				new AuthObjectIdentifier(4), "9876543210".getBytes("UTF-8"),
-				"PUK");
-		mf.addChild(puk);
+	public String getEidDg18PlainData() {
+		return "02760509130000";
 	}
-	
+
 	@Override
-	protected void addEpassDatagroup1(DedicatedFile ePassAppl) {
-		// ePass DG1
-		CardFile epassDg1 = new ElementaryFile(
-				new FileIdentifier(0x0101),
-				new ShortFileIdentifier(0x01),
-				HexString.toByteArray("615D5F1F5A4944443C3C303132333435363738343C3C3C3C3C3C3C3C3C3C3C3C3C3C3C333930323034364631303132333132443C3C3C3C3C3C3C3C3C3C3C3C3C344D55454C4C45523C3C48494C4445474152443C3C3C3C3C3C3C3C3C3C3C3C"),
-				Arrays.asList((SecCondition) new PaceSecurityCondition()),
-				Collections.<SecCondition> emptySet(),
-				Collections.<SecCondition> emptySet());
-		ePassAppl.addChild(epassDg1);
+	public String getDocumentNumber() {
+		return "000000001";
+	}
+
+	@Override
+	public String getMrzLine3of3() {
+		return "MUELLER<<HILDEGARD<<<<<<<<<<<<";
 	}
 
 }
