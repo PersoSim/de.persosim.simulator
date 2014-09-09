2ff60c8fb696f60019dafbc06856c4cb2ee2913f
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2014-09-09 09:31:31 +0200

    modified tests to match recently changed class names

--- de.persosim.simulator.test/gtTests/de/persosim/simulator/test/globaltester/perso/DefaultPersoTestPkiTemplate11Test.java
+++ de.persosim.simulator.test/gtTests/de/persosim/simulator/test/globaltester/perso/DefaultPersoTestPkiTemplate11Test.java
@@ -1,6 +1,6 @@
 package de.persosim.simulator.test.globaltester.perso;
 
-import de.persosim.simulator.perso.DefaultPersoTestPkiTemplate11;
+import de.persosim.simulator.perso.DefaultPersoTestPkiTemplate10;
 import de.persosim.simulator.perso.Personalization;
 
 public class DefaultPersoTestPkiTemplate11Test extends TestPersoTest {
@@ -8,7 +8,7 @@ public class DefaultPersoTestPkiTemplate11Test extends TestPersoTest {
 	@Override
 	public Personalization getPersonalization() {
 		if(persoCache == null) {
-			persoCache = new DefaultPersoTestPkiTemplate11();
+			persoCache = new DefaultPersoTestPkiTemplate10();
 		}
 		
 		return persoCache;
