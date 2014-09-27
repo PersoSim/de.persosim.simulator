3bd8141f51ebb9f0169f29e8f50d8a412123764e
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2014-09-27 17:45:37 +0200

    added handler for perso selection

---
+++ de.persosim.simulator.ui/src/de/persosim/simulator/ui/handlers/PersoSelectionHandler.java
@@ -0,0 +1,19 @@
+package de.persosim.simulator.ui.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * @author slutters
+ *
+ */
+public class PersoSelectionHandler {
+	
+	@Execute
+	public void execute(Shell shell) {
+		
+		
+		
+	}
+
+}
