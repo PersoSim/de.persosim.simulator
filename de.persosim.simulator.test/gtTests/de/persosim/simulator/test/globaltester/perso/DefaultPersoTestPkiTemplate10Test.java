67c8a44f3fab37f7fa4710a1fd28f29e917d0fdd
Alexander May <alexander.may@hjp-consulting.com>
2014-09-29 15:26:06 +0200

    Remove obsolete Template 10Bdr

--- de.persosim.simulator.test/gtTests/de/persosim/simulator/test/globaltester/perso/DefaultPersoTestPkiTemplate10Test.java
+++ de.persosim.simulator.test/gtTests/de/persosim/simulator/test/globaltester/perso/DefaultPersoTestPkiTemplate10Test.java
@@ -1,6 +1,6 @@
 package de.persosim.simulator.test.globaltester.perso;
 
-import de.persosim.simulator.perso.DefaultPersoTestPkiTemplate10Bdr;
+import de.persosim.simulator.perso.DefaultPersoTestPkiTemplate10;
 import de.persosim.simulator.perso.Personalization;
 
 public class DefaultPersoTestPkiTemplate10Test extends TestPersoTest {
@@ -8,7 +8,7 @@ public class DefaultPersoTestPkiTemplate10Test extends TestPersoTest {
 	@Override
 	public Personalization getPersonalization() {
 		if(persoCache == null) {
-			persoCache = new DefaultPersoTestPkiTemplate10Bdr();
+			persoCache = new DefaultPersoTestPkiTemplate10();
 		}
 		
 		return persoCache;
