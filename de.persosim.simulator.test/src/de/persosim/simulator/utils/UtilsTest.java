430aa9af88c185e2f27d415cbb74014ad85b4c7b
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2014-10-02 09:15:06 +0200

    UtilsTest: added tests

--- de.persosim.simulator.test/src/de/persosim/simulator/utils/UtilsTest.java
+++ de.persosim.simulator.test/src/de/persosim/simulator/utils/UtilsTest.java
@@ -1,8 +1,11 @@
 package de.persosim.simulator.utils;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import java.math.BigInteger;
+import java.util.Calendar;
+import java.util.Date;
 
 import org.junit.Test;
 
@@ -13,6 +16,132 @@ public class UtilsTest {
 		byte[] exp = new byte[]{(byte) 0x00};
 		byte[] recv = Utils.toUnsignedByteArray(BigInteger.ZERO);
 		assertArrayEquals(exp, recv);
+	}
+	
+	/**
+	 * Positive test case: parse complete and valid date String for all byte parameters.
+	 * For a complete and valid date String the results for all byte parameters are expected to be identical.
+	 */
+	@Test
+	public void testGetDateStringByte_CompleteString() {
+		Date dateReceivedMin = Utils.getDate("19640229", (byte) -1);
+		Date dateReceivedNorm = Utils.getDate("19640229", (byte) 0);
+		Date dateReceivedMax = Utils.getDate("19640229", (byte) 1);
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, 1964);
+		calendar.set(Calendar.MONTH, Calendar.FEBRUARY);
+		calendar.set(Calendar.DATE, 29);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		
+		Date dateExpected = calendar.getTime();
+		
+		assertEquals(dateExpected, dateReceivedMin);
+		assertEquals(dateExpected, dateReceivedNorm);
+		assertEquals(dateExpected, dateReceivedMax);
+	}
+	
+	/**
+	 * Negative test case: parse date String shorter than expected.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetDateStringByte_StringTooShort() {
+		Utils.getDate("1964022", (byte) 0);
+	}
+	
+	/**
+	 * Negative test case: parse date String longer than expected.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetDateStringByte_StringTooLongt() {
+		Utils.getDate("196402290", (byte) 0);
+	}
+	
+	/**
+	 * Positive test case: parse date String with incomplete month to be rounded down.
+	 */
+	@Test
+	public void testGetDateStringByte_IncompleteMonthRoundDown() {
+		Date dateReceived = Utils.getDate("1964XX29", (byte) -1);
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, 1964);
+		calendar.set(Calendar.MONTH, Calendar.JANUARY);
+		calendar.set(Calendar.DATE, 29);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		
+		Date dateExpected = calendar.getTime();
+		
+		assertEquals(dateExpected, dateReceived);
+	}
+	
+	/**
+	 * Positive test case: parse date String with incomplete month to be rounded up.
+	 */
+	@Test
+	public void testGetDateStringByte_IncompleteMonthRoundUp() {
+		Date dateReceived = Utils.getDate("1964XX29", (byte) 1);
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, 1964);
+		calendar.set(Calendar.MONTH, Calendar.DECEMBER);
+		calendar.set(Calendar.DATE, 29);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		
+		Date dateExpected = calendar.getTime();
+		
+		assertEquals(dateExpected, dateReceived);
+	}
+	
+	/**
+	 * Positive test case: parse date String with incomplete month to be rounded down.
+	 */
+	@Test
+	public void testGetDateStringByte_IncompleteDayRoundDown() {
+		Date dateReceived = Utils.getDate("196402XX", (byte) -1);
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, 1964);
+		calendar.set(Calendar.MONTH, Calendar.FEBRUARY);
+		calendar.set(Calendar.DATE, 1);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		
+		Date dateExpected = calendar.getTime();
+		
+		assertEquals(dateExpected, dateReceived);
+	}
+	
+	/**
+	 * Positive test case: parse date String with incomplete month to be rounded down.
+	 */
+	@Test
+	public void testGetDateStringByte_IncompleteDayRoundUp() {
+		Date dateReceived = Utils.getDate("196402XX", (byte) 1);
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, 1964);
+		calendar.set(Calendar.MONTH, Calendar.FEBRUARY);
+		calendar.set(Calendar.DATE, 29);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		
+		Date dateExpected = calendar.getTime();
+		
+		assertEquals(dateExpected, dateReceived);
 	}
 
 }
