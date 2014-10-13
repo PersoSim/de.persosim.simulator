aadad337eb0c513587b4fdba878e6882283bef83
Alexander May <alexander.may@hjp-consulting.com>
2014-10-13 16:22:40 +0200

    better method names for tests

--- de.persosim.simulator.test/src/de/persosim/simulator/tlv/TlvDataObjectTest.java
+++ de.persosim.simulator.test/src/de/persosim/simulator/tlv/TlvDataObjectTest.java
@@ -13,7 +13,7 @@ public class TlvDataObjectTest implements TlvConstants {
 	 * Positive test: PrimitiveTlvDataObject should be represented identical to it's toString()
 	 */
 	@Test
-	public void testGetTlvDataObject_singlePrimitiveObject() {
+	public void testDumpTlvDataObject_singlePrimitiveObject() {
 		TlvDataObject obj = TlvDataObjectFactory.createTLVDataObject("020101");
 		String expected = obj.toString();
 		assertEquals(expected, TlvDataObject.dumpTlvObject(obj));
@@ -23,7 +23,7 @@ public class TlvDataObjectTest implements TlvConstants {
 	 * Negative test: return an empty String on null parameter
 	 */
 	@Test
-	public void testGetTlvDataObject_nullArgument() {
+	public void testDumpTlvDataObject_nullArgument() {
 		TlvDataObject obj = null;
 		String expected = "";
 		assertEquals(expected, TlvDataObject.dumpTlvObject(obj));
@@ -33,7 +33,7 @@ public class TlvDataObjectTest implements TlvConstants {
 	 * Positive test: ConstructedTlvDataObject with no child at all
 	 */
 	@Test
-	public void testGetTlvDataObject_constructedObjectNoChild() {
+	public void testDumpTlvDataObject_constructedObjectNoChild() {
 		TlvDataObject obj = TlvDataObjectFactory.createTLVDataObject("3000");
 		String expected = obj.toString();
 		assertEquals(expected, TlvDataObject.dumpTlvObject(obj));
@@ -43,7 +43,7 @@ public class TlvDataObjectTest implements TlvConstants {
 	 * Positive test: Simple ConstructedTlvDataObject with one primitive child
 	 */
 	@Test
-	public void testGetTlvDataObject_constructedObjectOneChild() {
+	public void testDumpTlvDataObject_constructedObjectOneChild() {
 		TlvDataObject obj = TlvDataObjectFactory.createTLVDataObject("3003020101");
 		String expected = "30|03|" +
 		INDENT1+"02|01|01";
@@ -54,7 +54,7 @@ public class TlvDataObjectTest implements TlvConstants {
 	 * Positive test: Simple ConstructedTlvDataObject with two primitive children
 	 */
 	@Test
-	public void testGetTlvDataObject_constructedObjectTwoChildren() {
+	public void testDumpTlvDataObject_constructedObjectTwoChildren() {
 		TlvDataObject obj = TlvDataObjectFactory.createTLVDataObject("3006020101020102");
 		String expected = "30|06|" +
 			INDENT1 + "02|01|01" + 
@@ -66,7 +66,7 @@ public class TlvDataObjectTest implements TlvConstants {
 	 * Positive test: ConstructedTlvDataObject with one constructed child, which has two primitive children
 	 */
 	@Test
-	public void testGetTlvDataObject_constructedObjectConstructedChildTwoChildren() {
+	public void testDumpTlvDataObject_constructedObjectConstructedChildTwoChildren() {
 		TlvDataObject obj = TlvDataObjectFactory.createTLVDataObject("30083006020101020102");
 		String expected = "30|08|" +
 		    INDENT1 + "30|06|" +
@@ -79,7 +79,7 @@ public class TlvDataObjectTest implements TlvConstants {
 	 * Positive test: ConstructedTlvDataObject with one constructed child and primitive children afterwards
 	 */
 	@Test
-	public void testGetTlvDataObject_complexConstructedObject() {
+	public void testDumpTlvDataObject_complexConstructedObject() {
 		TlvDataObject obj = TlvDataObjectFactory.createTLVDataObject("300B3006020101020102020101");
 		String expected = "30|0B|" +
 			    INDENT1 + "30|06|" +
