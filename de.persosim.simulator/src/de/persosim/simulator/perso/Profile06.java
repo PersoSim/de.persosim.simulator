f72f1bfb903c3c8f4dccd27f0d50f27458534cab
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2014-10-21 13:06:53 +0200

    removed deviating TA trust point from "Profile06" and moved it to new profile "Profile01Tr03124". "Profile01Tr03124" is a subclass of original "Profile01" only overriding the setting of the TA trust point

--- de.persosim.simulator/src/de/persosim/simulator/perso/Profile06.java
+++ de.persosim.simulator/src/de/persosim/simulator/perso/Profile06.java
@@ -1,15 +1,6 @@
 package de.persosim.simulator.perso;
 
-import de.persosim.simulator.cardobjects.TrustPointCardObject;
-import de.persosim.simulator.cardobjects.TrustPointIdentifier;
 import de.persosim.simulator.crypto.CryptoUtil;
-import de.persosim.simulator.crypto.certificates.CardVerifiableCertificate;
-import de.persosim.simulator.exception.CertificateNotParseableException;
-import de.persosim.simulator.protocols.TR03110Utils;
-import de.persosim.simulator.protocols.ta.TerminalType;
-import de.persosim.simulator.tlv.ConstructedTlvDataObject;
-import de.persosim.simulator.tlv.TlvDataObject;
-import de.persosim.simulator.tlv.TlvDataObjectContainer;
 import de.persosim.simulator.utils.HexString;
 
 /**
@@ -21,14 +12,14 @@ public class Profile06 extends AbstractProfile {
 	@Override
 	public void setPersoDataContainer() {
 		persoDataContainer = new PersonalizationDataDefaultContainer();
-		persoDataContainer.setDg4PlainData("Hans-Günther");
-		persoDataContainer.setDg5PlainData("von Drebenbusch-Dalgoßen");
-		persoDataContainer.setDg6PlainData("Freiherr zu Möckern-Windensberg");
+		persoDataContainer.setDg4PlainData("Hans-Gï¿½nther");
+		persoDataContainer.setDg5PlainData("von Drebenbusch-Dalgoï¿½en");
+		persoDataContainer.setDg6PlainData("Freiherr zu Mï¿½ckern-Windensberg");
 		persoDataContainer.setDg7PlainData("Dr.eh.Dr.");
 		persoDataContainer.setDg8PlainData("19460125");
 		persoDataContainer.setDg9PlainData("BREMERHAVEN");
 		persoDataContainer.setDg11PlainData("M"); //XXX this is needed in order to make MRZ generation work but should not be included in data groups
-		persoDataContainer.setDg13PlainData("Weiß");
+		persoDataContainer.setDg13PlainData("Weiï¿½");
 		persoDataContainer.setDg17StreetPlainData("WEG NR. 12 8E");
 		persoDataContainer.setDg17CityPlainData("HAMBURG");
 		persoDataContainer.setDg17CountryPlainData("D");
@@ -60,24 +51,6 @@ public class Profile06 extends AbstractProfile {
 				HexString.toByteArray("0439036762A5736E37A55D4F4E875CAB87744DB74ADD255BA42C6533729AF1D95AA6AF1B264E53C08CD5FAE58684F462BD2AC6E6CFDFD8ADCB6BA65894439AA6FA"),
 				HexString.toByteArray("0640C5FB16B6083DF60DE4A00B231B22ED012FC672E5EE2849CAB6FB93CA947C")),
 				2);
-	}
-	
-	@Override
-	protected void addTaTrustPoints() throws CertificateNotParseableException {
-		// certificates
-		byte[] cvcaAtData = HexString
-				.toByteArray("7F218201BA7F4E8201725F2901004210444543564341654944435430303030317F4982011D060A04007F000702020202038120A9FB57DBA1EEA9BC3E660A909D838D726E3BF623D52620282013481D1F6E537782207D5A0975FC2C3057EEF67530417AFFE7FB8055C126DC5C6CE94A4B44F330B5D9832026DC5C6CE94A4B44F330B5D9BBD77CBF958416295CF7E1CE6BCCDC18FF8C07B68441048BD2AEB9CB7E57CB2C4B482FFC81B7AFB9DE27E1E3BD23C23A4453BD9ACE3262547EF835C3DAC4FD97F8461A14611DC9C27745132DED8E545C1D54C72F0469978520A9FB57DBA1EEA9BC3E660A909D838D718C397AA3B561A6F7901E0E82974856A78641047EC402F29B04079C9D89A8F732AD09BABC6538849128C6539B6C0F17EA4B72F56DD632376FA8CFD0E08E0DCA0F54802344F3137599121D20F9CADD358E5C3C7E8701015F2010444543564341654944435430303030317F4C12060904007F0007030102025305FE1FFFFFFF5F25060103000400075F24060106000400065F37406CB5D646D3F9AB8663946B7855CEAA589FD14BCC6917CE2FCFDD35AB3218DF16812EF181809FBFE53042424559D780618082765013F359991EE8216DF6C40E32");
-
-		TlvDataObject cvcaAtTlv = ((ConstructedTlvDataObject) new TlvDataObjectContainer(
-				cvcaAtData).getTlvDataObject(TR03110Utils.TAG_7F21))
-				.getTlvDataObject(TR03110Utils.TAG_7F4E);
-
-		// TA trustpoints
-		TrustPointCardObject trustPointAt = new TrustPointCardObject(
-				new TrustPointIdentifier(TerminalType.AT),
-				new CardVerifiableCertificate(
-						(ConstructedTlvDataObject) cvcaAtTlv));
-		mf.addChild(trustPointAt);
 	}
 
 }
