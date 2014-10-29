ee15adab39340c33cce1c78817a775c2fdcd75f6
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2014-10-29 10:02:53 +0100

    added JavaDoc as requested by review

--- de.persosim.simulator.ui/src/de/persosim/simulator/ui/handlers/SelectPersoHandler.java
+++ de.persosim.simulator.ui/src/de/persosim/simulator/ui/handlers/SelectPersoHandler.java
@@ -8,15 +8,30 @@ import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import de.persosim.simulator.PersoSim;
 import de.persosim.simulator.ui.parts.PersoSimGuiMain;
 
+/**
+ * This class implements an abstract super class for any handler handling the
+ * loading of personalizations providing shared code for the common goal.
+ * 
+ * @author slutters
+ *
+ */
 public abstract class SelectPersoHandler {
 	
 	@Inject
 	protected EPartService partService;
 	
-	protected void loadPersonalization(String param) {
-		System.out.println("Select Perso Handler called with param: " + param);
+	/**
+	 * This method sends the actual load command for a provided personalization
+	 * path or Id to the simulator GUI.
+	 * 
+	 * @param personalization
+	 *            the path (incl. file name) of an arbitrary personalization or
+	 *            the Id of a default personalization.
+	 */
+	protected void loadPersonalization(String personalization) {
+		System.out.println("Select Perso Handler called with param: " + personalization);
 		
-		String persoCmdString = PersoSim.CMD_LOAD_PERSONALIZATION + " " + param;
+		String persoCmdString = PersoSim.CMD_LOAD_PERSONALIZATION + " " + personalization;
 		
 		System.out.println("executing command: " + persoCmdString);
 		
