0f075455629c007c6cff7f41765b20f0aca8cd16
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2014-11-17 08:22:55 +0100

    extracted (class) variables, added lock for indirectly chained method calls

--- de.persosim.simulator.ui/src/de/persosim/simulator/ui/utils/TextLengthLimiter.java
+++ de.persosim.simulator.ui/src/de/persosim/simulator/ui/utils/TextLengthLimiter.java
@@ -5,30 +5,57 @@ import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.widgets.Text;
 
 public class TextLengthLimiter implements ModifyListener {
-
+	
+	public static final int TEXT_LIMIT_DEFAULT = 2000;
+	public static int counter = 0;
+	
+	protected int textLimit;
+	public boolean lock;
+	
+	public TextLengthLimiter() {
+		textLimit = TEXT_LIMIT_DEFAULT;
+		lock = false;
+		counter++;
+	}
+	
 	@Override
 	public void modifyText(ModifyEvent e) {
-		if(e.getSource() instanceof Text) {
+		if((!lock) && (e.getSource() instanceof Text)) {
 			Text text = (Text) e.getSource();
-			int textLimit = 1000;
 			String lineDelimiter = text.getLineDelimiter();
 			
-			String message = text.getMessage();
+			String content = text.getText();
 			
-			if(message.length() > textLimit) {
-				do {
-					int index = message.indexOf(lineDelimiter);
-					if(index >= 0) {
-						message = message.substring(index + 1);
-					} else {
-						message = message.substring(message.length() - textLimit);
-					}
-				} while(message.length() > textLimit);
+			int contentLength = content.length();
+			if(contentLength > textLimit) {
+				lock = true;
 				
-				text.setText(message);
+				do {
+					int index = content.indexOf(lineDelimiter);
+					if(index >= 0) {
+						content = content.substring(index + 1);
+					} else {
+						content = content.substring(content.length() - textLimit);
+					}
+					
+					contentLength = content.length();
+				} while(contentLength > textLimit);
+				
+				text.setText(content);
+				System.out.println("!!! text shortened !!!");
+				
+				lock = false;
 			}
 		}
 		
+	}
+
+	public int getTextLimit() {
+		return textLimit;
+	}
+
+	public void setTextLimit(int textLimit) {
+		this.textLimit = textLimit;
 	}
 
 }
