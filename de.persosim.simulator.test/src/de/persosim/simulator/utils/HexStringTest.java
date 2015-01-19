cd52b94304f5af3c7565c9d68e855818060d0ce7
Lukas Stauffenberg <lukas.stauffenberg@hjp-consulting.com>
2015-01-19 10:57:54 +0100

    Added new Testcases for the HexStringTest file.

--- de.persosim.simulator.test/src/de/persosim/simulator/utils/HexStringTest.java
+++ de.persosim.simulator.test/src/de/persosim/simulator/utils/HexStringTest.java
@@ -1,6 +1,7 @@
 package de.persosim.simulator.utils;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -16,7 +17,7 @@ public class HexStringTest {
 	}
 	
 	/**
-	 * Positive test case: simple byte 00
+	 * Positive test case: convert a simple byte with the value 00
 	 */
 	@Test
 	public void testToByteArray_Value_00() {
@@ -46,38 +47,24 @@ public class HexStringTest {
 	}
 	
 	/**
-	 * Negative test case: get IllegalArgumentException because of odd value FIXME LSG tis is not an odd value, but a string with odd length
-	 * entered
+	 * Negative test case: get IllegalArgumentException because 
+	 * of string with odd length entered
 	 */
-	@Test
-	public void testToByteArray_ODD_Value() {
-		String s = "ABC";
-
-		boolean thrown = false;
-
-		 //FIXME LSG we won't merge warnings on master ;-)
-		byte[] exp = new byte[] { (byte) 0x0A, (byte) 0xBC };
-		byte[] recv = null;
-		try {
-			//FIXME LSG while this implementation, (catching and expecting an exception) is technically correct JUnit proivdes better means, search for "JUnit expected exception"
-
-			recv = HexString.toByteArray(s);
-		} catch (IllegalArgumentException e) {
-
-			thrown = true;
-
-		}
-		assertTrue(thrown);
-
+	@Test(expected=IllegalArgumentException.class)
+	public void IllegalArgumentException() {
+//		String s ="ABC";
+//		byte[] c = HexString.toByteArray(s);
+		HexString.toByteArray("ABC");
+		
 	}
-	
+		
 	/**
 	 * Positive test case: convert empty string to byte array
 	 * 
 	 */
 	@Test
 	public void testToByteArray_Empty_Value() {
-		String s = " "; //FIXME LSG this is not an empty String!!
+		String s = ""; 
 		byte[] exp = new byte[]{};
 		byte[] recv = HexString.toByteArray(s);
 		
@@ -85,7 +72,20 @@ public class HexStringTest {
 	}
 	
 	/**
-	 * Positive test case: convert empty string to byte array //FIXME LSG this description does not match the method content
+	 * Positive test case: convert space to byte array
+	 * 
+	 */
+	@Test
+	public void testToByteArray_SPACE_Value() {
+		String s = " "; 
+		byte[] exp = new byte[]{};
+		byte[] recv = HexString.toByteArray(s);
+		
+		assertArrayEquals(exp, recv);
+	}
+	
+	/**
+	 * Positive test case: convert the singlebyte F5
 	 * 
 	 */
 	@Test
@@ -97,36 +97,6 @@ public class HexStringTest {
 		assertArrayEquals(exp, recv);
 	}
 	
-	/**
-	 * Positive test case: convert a very long "F" string to byte array
-	 */
-	@Test
-	public void testToByteArray_Value_FF() {
-		//FIXME LSG this description of the input and expected result is ugly. Try to describe them in one simple loop or similar.
-		String s = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
-				+ "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
-				+ "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
-		byte[] exp = new byte[] { (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-				(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-				(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-				(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-				(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-				(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-				(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-				(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-				(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-				(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-				(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-				(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-				(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-				(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-				(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-				(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF };
-
-		byte[] recv = HexString.toByteArray(s);
-
-		assertArrayEquals(exp, recv);
-	}
 	
 	/**
 	 * Positive test case: convert String to byte array for which the most
