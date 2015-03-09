3edc811be5395d387ab76a4f05f73cc781fe95ae
Martin Boonk <martin.boonk@hjp-consulting.com>
2015-03-09 16:47:16 +0100

    Fixed the wrong interfaces beeing used for the service tracker type arguments

--- de.persosim.simulator/src/de/persosim/simulator/Activator.java
+++ de.persosim.simulator/src/de/persosim/simulator/Activator.java
@@ -2,23 +2,22 @@ package de.persosim.simulator;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.service.log.LogReaderService;
 import org.osgi.service.log.LogService;
 import org.osgi.util.tracker.ServiceTracker;
 
 public class Activator implements BundleActivator {
-	private static ServiceTracker<LogReaderService, LogReaderService> logServiceTracker;
+	private static ServiceTracker<LogService, LogService> logServiceTracker;
 	
-	public static LogService getLogservice() {
+	public static LogService getLogservice() {		
 		if (logServiceTracker != null){
-			return (LogService) logServiceTracker.getService();	
+			return logServiceTracker.getService();
 		}
 		return null;
 	}
 
 	@Override
 	public void start(BundleContext context) throws Exception {
-		logServiceTracker = new ServiceTracker<LogReaderService, LogReaderService>(context, LogService.class.getName(), null);
+		logServiceTracker = new ServiceTracker<LogService, LogService>(context, LogService.class.getName(), null);
         logServiceTracker.open();
 
 	}
