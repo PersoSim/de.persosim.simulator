a5c266d1035eddc51370a0469d51fc28eeaa15e2
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2015-03-27 16:14:51 +0100

    register crypto provider

--- de.persosim.simulator/src/de/persosim/simulator/Activator.java
+++ de.persosim.simulator/src/de/persosim/simulator/Activator.java
@@ -1,13 +1,27 @@
 package de.persosim.simulator;
 
+import java.security.Provider;
+import java.security.Security;
+import java.util.Hashtable;
+
+import org.globaltester.cryptoprovider.Cryptoprovider;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.log.LogService;
 import org.osgi.util.tracker.ServiceTracker;
 
 public class Activator implements BundleActivator {
+
+	public static BundleContext context;
+	
+	private static Provider cryptoProvider;
+	public static Cryptoprovider objectImplementingInterface;
+	
 	private static ServiceTracker<LogService, LogService> logServiceTracker;
-	private static BundleContext context;
+
+	
+	private ServiceTracker<?, ?> serviceTracker;
+	
 	
 	public static LogService getLogservice() {		
 		if (logServiceTracker != null){
@@ -19,8 +33,16 @@ public class Activator implements BundleActivator {
 	@Override
 	public void start(BundleContext context) throws Exception {
 		Activator.context = context;
+		
+		//get LogService
 		logServiceTracker = new ServiceTracker<LogService, LogService>(context, LogService.class.getName(), null);
         logServiceTracker.open();
+		
+		registerCryptoProvider();
+
+		//Registers Simulator service
+		context.registerService(Simulator.class.getName(), new PersoSim(), new Hashtable<String, String>());
+		
 	}
 
 	@Override
@@ -32,4 +54,27 @@ public class Activator implements BundleActivator {
 	public static BundleContext getContext() {
 		return context;
 	}
+	
+	public void registerCryptoProvider() {
+		serviceTracker = new ServiceTracker<>(context, Cryptoprovider.class, null);
+		serviceTracker.open();
+		
+		Object[] allServiceObjects = serviceTracker.getServices();
+		System.out.println("service tracker tracking " + allServiceObjects.length + " service objects");
+		for(Object currentServiceObject : allServiceObjects) {
+			System.out.println("service object: " + currentServiceObject.getClass().getName());
+		}
+		
+		objectImplementingInterface = (Cryptoprovider) serviceTracker.getService();
+		
+		System.out.println("objectImplementingInterface: " + objectImplementingInterface);
+		
+		cryptoProvider = objectImplementingInterface.getCryptoProviderObject();
+		
+		System.out.println("cryptoProvider: " + cryptoProvider);
+		
+		// register BouncyCastle provider
+		Security.addProvider(cryptoProvider);
+	}
+	
 }
