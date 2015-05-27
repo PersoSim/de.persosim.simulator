f0853bfecf6f299ea553e3e1bb0003b604d94cc5
Martin Boonk <martin.boonk@hjp-consulting.com>
2015-05-27 11:19:51 +0200

    Added a customization interface to the TR03110 static helper class

---
+++ de.persosim.simulator/src/de/persosim/simulator/protocols/TR03110UtilsProvider.java
@@ -0,0 +1,17 @@
+package de.persosim.simulator.protocols;
+
+import java.security.GeneralSecurityException;
+import java.security.Key;
+import java.security.PublicKey;
+
+import de.persosim.simulator.crypto.DomainParameterSet;
+import de.persosim.simulator.tlv.ConstructedTlvDataObject;
+
+public interface TR03110UtilsProvider {
+
+	PublicKey parsePublicKey(ConstructedTlvDataObject publicKeyData,
+			PublicKey trustPointPublicKey) throws GeneralSecurityException;
+
+	DomainParameterSet getDomainParameterSetFromKey(Key key);
+
+}
