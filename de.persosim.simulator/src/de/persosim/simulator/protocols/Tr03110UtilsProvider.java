b73297af7ae75679fe93779a1fee6d78ff03293f
Martin Boonk <martin.boonk@hjp-consulting.com>
2015-06-03 16:59:26 +0200

    Added documentation

--- de.persosim.simulator/src/de/persosim/simulator/protocols/Tr03110UtilsProvider.java
+++ de.persosim.simulator/src/de/persosim/simulator/protocols/Tr03110UtilsProvider.java
@@ -7,11 +7,35 @@ import java.security.PublicKey;
 import de.persosim.simulator.crypto.DomainParameterSet;
 import de.persosim.simulator.tlv.ConstructedTlvDataObject;
 
+/**
+ * This interface are used to modularize the {@link Tr03110Utils} class.
+ * 
+ * @author mboonk
+ *
+ */
 public interface Tr03110UtilsProvider {
 
+	/**
+	 * This methods parses a given TLV-encoded public key using the domain
+	 * parameters form a given public key. This is usually the key of a trust
+	 * point.
+	 * 
+	 * @param publicKeyData
+	 *            the TLV data to parse
+	 * @param trustPointPublicKey
+	 *            the key containing domain parameters or null if the public key
+	 *            data contains domain parameters
+	 * @return the public key or null if the key data is not supported
+	 * @throws GeneralSecurityException
+	 */
 	PublicKey parsePublicKey(ConstructedTlvDataObject publicKeyData,
 			PublicKey trustPointPublicKey) throws GeneralSecurityException;
 
+	/**
+	 * This creates domain parameters from a given key object.
+	 * @param key the key to extract domain parameters from
+	 * @return the domain parameters or null if the key does not contain supported domain parameters
+	 */
 	DomainParameterSet getDomainParameterSetFromKey(Key key);
 
 }
