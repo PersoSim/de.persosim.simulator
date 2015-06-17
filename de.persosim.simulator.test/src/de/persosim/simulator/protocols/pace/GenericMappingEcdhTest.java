73667868b614659e4fee413573b2da90d9ec1fb3
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2015-06-17 12:17:44 +0200

    simplified unit tests

--- de.persosim.simulator.test/src/de/persosim/simulator/protocols/pace/GenericMappingEcdhTest.java
+++ de.persosim.simulator.test/src/de/persosim/simulator/protocols/pace/GenericMappingEcdhTest.java
@@ -9,8 +9,6 @@ import java.math.BigInteger;
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.interfaces.ECPrivateKey;
 import java.security.interfaces.ECPublicKey;
@@ -198,52 +196,38 @@ public class GenericMappingEcdhTest extends PersoSimTestCase {
 		DomainParameterSetEcdh domainParameterSetUnMapped = (DomainParameterSetEcdh) StandardizedDomainParameters.getDomainParameterSetById(13);
 		GenericMappingEcdh mapping = new GenericMappingEcdh();
 		
-		BigInteger privateKeyComponentPicc = new BigInteger(1, HexString.toByteArray("7FC3DE0EDE951E6181392527612FF2A50D4E6C6FE00F7A92E66CB3D7B7D23044"));
+		byte[] privateKeyDataPicc = HexString.toByteArray("7FC3DE0EDE951E6181392527612FF2A50D4E6C6FE00F7A92E66CB3D7B7D23044");
+		byte[] publicKeyDataPcd = HexString.toByteArray("0424EF5B5C5D5F085783357C34C01660C6A062005BA1E347EB5E890DC34A305085161950814AE4D7BF20137D5C425E039CCC250835D69E8FEE92E302F468F39394");
 		
-		BigInteger publicKeyComponentPcdx = new BigInteger(1, HexString.toByteArray("24EF5B5C5D5F085783357C34C01660C6A062005BA1E347EB5E890DC34A305085"));
-		BigInteger publicKeyComponentPcdy = new BigInteger(1, HexString.toByteArray("161950814AE4D7BF20137D5C425E039CCC250835D69E8FEE92E302F468F39394"));
-		ECPoint publicKeyComponentPcd = new ECPoint(publicKeyComponentPcdx, publicKeyComponentPcdy);
-		
-		ECPrivateKeySpec privateKeySpec = domainParameterSetUnMapped.getPrivateKeySpec(privateKeyComponentPicc);
-		ECPublicKeySpec publicKeySpec = domainParameterSetUnMapped.getPublicKeySpec(publicKeyComponentPcd);
-		
-		KeyFactory keyFactory = KeyFactory.getInstance(domainParameterSetUnMapped.getKeyAgreementAlgorithm());
-		PrivateKey privKeyPicc = keyFactory.generatePrivate(privateKeySpec);
-		PublicKey pubKeyPcd = keyFactory.generatePublic(publicKeySpec);
+		ECPrivateKey ecPrivKeyPicc = domainParameterSetUnMapped.reconstructPrivateKey(privateKeyDataPicc);
+		ECPublicKey ecPubKeyPcd = domainParameterSetUnMapped.reconstructPublicKey(publicKeyDataPcd);
 		
 		byte[] commonSecretExpected = HexString.toByteArray("04326C2CE38AC366142735AFA4317A24BDE8F12AFAEE1575CE9756E3A8849F9AEF30103CF5396CBA2F4678572988513CFC0F0CBE116644A5B9E8C6B229E0C9E2FB");
 		
-		byte[] commonSecretReceived = mapping.performKeyAgreement(domainParameterSetUnMapped, privKeyPicc, pubKeyPcd);
+		byte[] commonSecretReceived = mapping.performKeyAgreement(domainParameterSetUnMapped, ecPrivKeyPicc, ecPubKeyPcd);
 		
 		assertArrayEquals("common mapping secret", commonSecretExpected, commonSecretReceived);
 	}
 	
 	/**
-	 * Negative test case: perform key agreement as part of mapping function based on values from valid PACE test run but with wrong PCD public key.
+	 * Negative test case: perform key agreement as part of mapping function based on values from valid PACE test run but with wrong PCD public key (on curve but not matching).
 	 * @throws InvalidKeySpecException 
 	 * @throws NoSuchAlgorithmException 
 	 */
 	@Test
-	public void testPerformKeyAgreementX() throws InvalidKeySpecException, NoSuchAlgorithmException {
+	public void testPerformKeyAgreement_wrongPublicKey() throws InvalidKeySpecException, NoSuchAlgorithmException {
 		DomainParameterSetEcdh domainParameterSetUnMapped = (DomainParameterSetEcdh) StandardizedDomainParameters.getDomainParameterSetById(13);
 		GenericMappingEcdh mapping = new GenericMappingEcdh();
 		
-		BigInteger privateKeyComponentPicc = new BigInteger(1, HexString.toByteArray("7FC3DE0EDE951E6181392527612FF2A50D4E6C6FE00F7A92E66CB3D7B7D23044"));
+		byte[] privateKeyDataPicc = HexString.toByteArray("7FC3DE0EDE951E6181392527612FF2A50D4E6C6FE00F7A92E66CB3D7B7D23044");
+		byte[] publicKeyDataPcd = HexString.toByteArray("047307AED59C716B4328E974EC2460104E013E93B9826A47CB9DB8A104F493F685094776A48D5B6746058D2B0FB206B69E4AA16E8E893BB4908285482BC4B82232");
 		
-		BigInteger publicKeyComponentPcdx = new BigInteger(1, HexString.toByteArray("24EF5B5C5D5F085783357C34C01660C6A062005BA1E347EB5E890DC34A305086")); // wrong x-coordinate
-		BigInteger publicKeyComponentPcdy = new BigInteger(1, HexString.toByteArray("161950814AE4D7BF20137D5C425E039CCC250835D69E8FEE92E302F468F39394"));
-		ECPoint publicKeyComponentPcd = new ECPoint(publicKeyComponentPcdx, publicKeyComponentPcdy);
-		
-		ECPrivateKeySpec privateKeySpec = domainParameterSetUnMapped.getPrivateKeySpec(privateKeyComponentPicc);
-		ECPublicKeySpec publicKeySpec = domainParameterSetUnMapped.getPublicKeySpec(publicKeyComponentPcd);
-		
-		KeyFactory keyFactory = KeyFactory.getInstance(domainParameterSetUnMapped.getKeyAgreementAlgorithm());
-		PrivateKey privKeyPicc = keyFactory.generatePrivate(privateKeySpec);
-		PublicKey pubKeyPcd = keyFactory.generatePublic(publicKeySpec);
+		ECPrivateKey ecPrivKeyPicc = domainParameterSetUnMapped.reconstructPrivateKey(privateKeyDataPicc);
+		ECPublicKey ecPubKeyPcd = domainParameterSetUnMapped.reconstructPublicKey(publicKeyDataPcd);
 		
 		byte[] commonSecretExpected = HexString.toByteArray("04326C2CE38AC366142735AFA4317A24BDE8F12AFAEE1575CE9756E3A8849F9AEF30103CF5396CBA2F4678572988513CFC0F0CBE116644A5B9E8C6B229E0C9E2FB");
 		
-		byte[] commonSecretReceived = mapping.performKeyAgreement(domainParameterSetUnMapped, privKeyPicc, pubKeyPcd);
+		byte[] commonSecretReceived = mapping.performKeyAgreement(domainParameterSetUnMapped, ecPrivKeyPicc, ecPubKeyPcd);
 		
 		assertFalse("common mapping secret", Arrays.equals(commonSecretExpected, commonSecretReceived));
 	}
