965b6d780c8792277490f3562defabcf5e6db159
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2015-09-07 15:41:04 +0200

    added documentation

--- de.persosim.simulator/src/de/persosim/simulator/apdu/InterindustryCommandApdu.java
+++ de.persosim.simulator/src/de/persosim/simulator/apdu/InterindustryCommandApdu.java
@@ -1,10 +1,25 @@
 package de.persosim.simulator.apdu;
 
-//FIXME SLS Interfaces need JavaDoc for all methods
+/**
+ * This interface extends {@link CommandApdu} by adding further functionality
+ * relevant in the context of interindustry command APDUs represented by this
+ * interface.
+ * 
+ * @author slutters
+ * 
+ */
 public interface InterindustryCommandApdu extends CommandApdu, IsoSecureMessagingCommandApdu {
-
+	
+	/**
+	 * This method returns true iff chaining is set for this command APDU, otherwise false is returned
+	 * @return true iff chaining is set for this command APDU, otherwise false is returned
+	 */
 	boolean isChaining();
-
+	
+	/**
+	 * This method returns the channel which is set for this command APDU
+	 * @return the channel which is set for this command APDU
+	 */
 	byte getChannel();
 
 }
