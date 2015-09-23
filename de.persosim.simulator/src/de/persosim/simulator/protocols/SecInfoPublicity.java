c94bc45594872186844277a73ebcf25e39c47093
Martin Boonk <martin.boonk@hjp-consulting.com>
2015-09-23 10:29:28 +0200

    Review
    *added some documentation

--- de.persosim.simulator/src/de/persosim/simulator/protocols/SecInfoPublicity.java
+++ de.persosim.simulator/src/de/persosim/simulator/protocols/SecInfoPublicity.java
@@ -1,6 +1,16 @@
 package de.persosim.simulator.protocols;
 
 
+/**
+ * This contains the different level of publicity for the security infos
+ * returned by protocols. It is used when creating card files containing
+ * security infos by
+ * {@link Protocol#getSecInfos(SecInfoPublicity, de.persosim.simulator.cardobjects.MasterFile)}
+ * .
+ * 
+ * @author jgoeke
+ *
+ */
 public enum SecInfoPublicity {
 	
     PUBLIC,
