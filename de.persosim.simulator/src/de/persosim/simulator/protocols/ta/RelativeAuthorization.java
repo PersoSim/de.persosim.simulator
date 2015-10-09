8b544d5cf575c5c0d52dcc78e9148467e601fcc9
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2015-10-09 16:16:56 +0200

    slimmed down methods

--- de.persosim.simulator/src/de/persosim/simulator/protocols/ta/RelativeAuthorization.java
+++ de.persosim.simulator/src/de/persosim/simulator/protocols/ta/RelativeAuthorization.java
@@ -10,34 +10,16 @@ import de.persosim.simulator.utils.BitField;
  * 
  */
 public class RelativeAuthorization extends Authorization {
-	
-	protected CertificateRole role;
 
 	public RelativeAuthorization() {
 	}
 	
 	public RelativeAuthorization(CertificateRole role, BitField authorization) {
-		super(authorization);
-		this.role = role;
+		super(authorization.concatenate(role.getField()));
 	}
 	
 	public CertificateRole getRole() {
-		return role;
-	}
-	
-	public BitField getRepresentation() {
-		return authorization.concatenate(role.getField());
-	}
-
-	@Override
-	public RelativeAuthorization buildEffectiveAuthorization(Authorization authorization) {
-		if (authorization instanceof RelativeAuthorization){
-			BitField effectiveRole = ((RelativeAuthorization) authorization).getRole().getField().and(this.role.getField());
-			BitField effectiveAuth = super.buildEffectiveAuthorization(authorization).getAuthorization(); 
-			return new RelativeAuthorization(CertificateRole.getFromField(effectiveRole), effectiveAuth);	
-		}
-		
-		throw new IllegalArgumentException("parameter must be of type " + RelativeAuthorization.class.getName());
+		return CertificateRole.getFromMostSignificantBits(authorization);
 	}
 	
 }
