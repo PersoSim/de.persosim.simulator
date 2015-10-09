74dfbca778d75eabe6397c948e971fbd8a89986f
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2015-10-09 11:40:37 +0200

    updated AuthorizationMechanism to be based on AuthorizationStore

--- de.persosim.simulator/src/de/persosim/simulator/secstatus/AuthorizationMechanism.java
+++ de.persosim.simulator/src/de/persosim/simulator/secstatus/AuthorizationMechanism.java
@@ -1,16 +1,18 @@
 package de.persosim.simulator.secstatus;
 
-import java.util.HashMap;
-
 import de.persosim.simulator.protocols.Oid;
 import de.persosim.simulator.protocols.ta.Authorization;
 
 public class AuthorizationMechanism implements SecMechanism {
 	
-	private HashMap<Oid, Authorization> authorizations;
+	private AuthorizationStore authorizationStore;
 	
-	public AuthorizationMechanism(HashMap<Oid, Authorization> authorizations) {
-		this.authorizations = authorizations;
+	public AuthorizationMechanism(AuthorizationMechanism authorizationMechanism) {
+		authorizationStore = authorizationMechanism.getAuthorizationStore();
+	}
+	
+	public AuthorizationMechanism(AuthorizationStore authorizationStore) {
+		this.authorizationStore = authorizationStore.clone();
 	}
 	
 	@Override
@@ -23,11 +25,16 @@ public class AuthorizationMechanism implements SecMechanism {
 	}
 
 	public Authorization getAuthorization(Oid oid) {
-		return authorizations.get(oid);
+		return authorizationStore.getAuthorization(oid);
 	}
 	
-	public AuthorizationMechanism getUpdatedMechanism(Oid oid, Authorization authorization) {
-		return getUpdatedMechanism(this, oid, authorization);
+	public AuthorizationStore getAuthorizationStore() {
+		return authorizationStore.clone();
+	}
+	
+	@Override
+	public AuthorizationMechanism clone() {
+		return new AuthorizationMechanism(this);
 	}
 	
 	/**
@@ -35,25 +42,13 @@ public class AuthorizationMechanism implements SecMechanism {
 	 * In case the provided mechanism does not contain any authorization registered for the provided OID, the authorization is registered for the specific OID.
 	 * In case the provided mechanism contains an authorization registered for the provided OID, this authorization is replaced with the effective authorization resulting from both authorizations available.
 	 * @param authMechanism the mechanism to update
-	 * @param oid the OID to register the authorization
-	 * @param authorization the authorization to update
+	 * @param authorizationStore the authorization information to use for update
 	 * @return a copy of the provided {@link AuthorizationMechanism} updated with the also provided {@link Authorization}
 	 */
-	public static AuthorizationMechanism getUpdatedMechanism(AuthorizationMechanism authMechanism, Oid oid, Authorization authorization) {
-		HashMap<Oid, Authorization> newAuthorizations = new HashMap<>(authMechanism.authorizations);
-		
-		Authorization auth = authMechanism.authorizations.get(oid);
-		Authorization newAuthorization;
-		
-		if(auth == null) {
-			newAuthorization = authorization;
-		} else{
-			newAuthorization = auth.buildEffectiveAuthorization(authorization);
-		}
-		
-		newAuthorizations.put(oid, newAuthorization);
-		
-		return new AuthorizationMechanism(newAuthorizations);
+	public AuthorizationMechanism getUpdatedMechanism(AuthorizationMechanism authMechanism, AuthorizationStore authorizationStore) {
+		AuthorizationStore originalAuthStore = authMechanism.getAuthorizationStore();
+		originalAuthStore.updateAuthorization(authorizationStore);
+		return new AuthorizationMechanism(originalAuthStore);
 	}
 	
 }
