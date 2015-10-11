a5205e52860df38d05aece6c0d579fcd05bbca83
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2015-10-11 12:46:03 +0200

    updated AuthorizationStore for full update checks

--- de.persosim.simulator/src/de/persosim/simulator/secstatus/AuthorizationStore.java
+++ de.persosim.simulator/src/de/persosim/simulator/secstatus/AuthorizationStore.java
@@ -13,25 +13,28 @@ private HashMap<Oid, Authorization> authorizations;
 		authorizations = new HashMap<>();
 	}
 	
-	public AuthorizationStore(AuthorizationStore authStore) {
-		this();
-		updateAuthorization(authStore);
-	}
-
 	public AuthorizationStore(HashMap<Oid, Authorization> authorizations) {
-		this.authorizations = authorizations;
+		this();
+		
+		for(Oid currentOid:authorizations.keySet()) {
+			authorizations.put(currentOid, authorizations.get(currentOid));
+		}
+	}
+	
+	public AuthorizationStore(AuthorizationStore authStore) {
+		this(authStore.authorizations);
 	}
 	
 	public Authorization getAuthorization(Oid oid) {
 		return authorizations.get(oid);
 	}
 	
-	public void updateAuthorization(Oid oid, Authorization authorization) {
+	private void updateAuthorization(Oid oid, Authorization authorization) {
 		Authorization auth = authorizations.get(oid);
 		Authorization newAuthorization;
 		
 		if(auth == null) {
-			newAuthorization = authorization;
+			newAuthorization = authorization.getMinimumAuthorization();
 		} else{
 			newAuthorization = auth.buildEffectiveAuthorization(authorization);
 		}
@@ -39,14 +42,20 @@ private HashMap<Oid, Authorization> authorizations;
 		authorizations.put(oid, newAuthorization);
 	}
 	
-	public void updateAuthorization(AuthorizationStore authStore) {
-		Authorization currentAuthorization;
-		
-		for(Oid currentOid:authStore.authorizations.keySet()) {
-			currentAuthorization = authStore.getAuthorization(currentOid);
-			updateAuthorization(currentOid, currentAuthorization);
+	public void updateAuthorization(HashMap<Oid, Authorization> authorizations) {
+		for(Oid currentOid:authorizations.keySet()) {
+			updateAuthorization(currentOid, authorizations.get(currentOid));
 		}
 		
+		for(Oid currentOid:this.authorizations.keySet()) {
+			if(authorizations.get(currentOid) == null) {
+				updateAuthorization(currentOid, this.authorizations.get(currentOid).getMinimumAuthorization());
+			}
+		}
+	}
+	
+	public void updateAuthorization(AuthorizationStore authStore) {
+		updateAuthorization(authStore.authorizations);
 	}
 	
 	@Override
