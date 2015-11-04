2cfc10706bb4c5f0e0e6b058315daf73161dcba6
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2015-11-04 09:00:06 +0100

    adapted to changes in Personalization interface

--- de.persosim.simulator/src/de/persosim/simulator/perso/PersonalizationImpl.java
+++ de.persosim.simulator/src/de/persosim/simulator/perso/PersonalizationImpl.java
@@ -3,57 +3,15 @@ package de.persosim.simulator.perso;
 import java.util.ArrayList;
 import java.util.List;
 
-import de.persosim.simulator.cardobjects.MasterFile;
 import de.persosim.simulator.platform.Layer;
-import de.persosim.simulator.protocols.Protocol;
 
 public class PersonalizationImpl implements Personalization {
 	
 	protected List<Layer> layers = null;
-	protected List<Protocol> protocols = null;
-	
-	protected MasterFile mf = null;
 	
 	public PersonalizationImpl() {
 		buildLayerList();
-		buildProtocolList();
-		buildObjectTree();
-	}
-
-	public MasterFile getMf() {
-		return mf;
-	}
-
-	@Override
-	public MasterFile getObjectTree() {
-		return getMf();
-	}
-
-	@Override
-	public List<Protocol> getProtocolList() {
-		return protocols;
-	}
-
-	/**
-	 * (Re)Build the protocol list (in {@link #protocols})
-	 * <p/>
-	 * This method is called from {@link #reset()} and should be implemented at
-	 * least in all Subclasses that are used within tests that need to reset the
-	 * personalization.
-	 */
-	protected void buildProtocolList() {
-		protocols = new ArrayList<>();
-	}
-	
-	/**
-	 * (Re)Build the Object tree (in {@link #mf})
-	 * <p/>
-	 * This method is called from {@link #reset()} and should be implemented at
-	 * least in all Subclasses that are used within tests that need to reset the
-	 * personalization.
-	 */
-	protected void buildObjectTree() {
-		mf = new MasterFile();
+		
 	}
 
 	@Override
@@ -70,6 +28,11 @@ public class PersonalizationImpl implements Personalization {
 	 */
 	protected void buildLayerList() {
 		layers = new ArrayList<>();
+	}
+
+	@Override
+	public void initialize() {
+		
 	}
 	
 }
