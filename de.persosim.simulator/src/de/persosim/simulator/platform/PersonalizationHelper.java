9271514c12b62a4a5982b6c7ae0b235a064cea50
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2015-11-04 11:14:02 +0100

    added class with helper methods for working on/with personalizations

---
+++ de.persosim.simulator/src/de/persosim/simulator/platform/PersonalizationHelper.java
@@ -0,0 +1,48 @@
+package de.persosim.simulator.platform;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import de.persosim.simulator.cardobjects.CardObject;
+import de.persosim.simulator.cardobjects.Iso7816LifeCycleState;
+import de.persosim.simulator.exception.LifeCycleChangeException;
+import de.persosim.simulator.utils.PersoSimLogger;
+
+/**
+ * This class provides various helper methods which may be useful when operating
+ * on/with personalizations.
+ * 
+ * @author slutters
+ *
+ */
+public class PersonalizationHelper {
+	
+	public static <T> Collection<? extends T> getCompatibleLayers(Collection<Layer> layers, Class<T> type){
+		HashSet<T> result = new HashSet<>();
+		
+		for (Layer layer : layers) {
+			if (type.isAssignableFrom(layer.getClass())){
+				@SuppressWarnings("unchecked")
+				T casted = (T)layer;
+				result.add(casted);
+			}
+		}
+		return result;
+	}
+	
+	public static void setLifeCycleStates(CardObject objectTree) {
+		Collection<CardObject> children = objectTree.getChildren();
+		if (children.size() > 0){
+			for (CardObject cardObject : children) {
+				setLifeCycleStates(cardObject);
+				if (cardObject.getLifeCycleState().isPersonalizationPhase()){
+					try {
+						cardObject.updateLifeCycleState(Iso7816LifeCycleState.OPERATIONAL_ACTIVATED);
+					} catch (LifeCycleChangeException e) {
+						PersoSimLogger.logException(PersonalizationHelper.class, e, PersoSimLogger.WARN);
+					}	
+				}
+			}
+		}
+	}
+}
