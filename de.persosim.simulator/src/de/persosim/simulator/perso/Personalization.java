23bed0542999f1a6106f54aabd069c5e7ba1c91a
Sascha Lutters <sascha.lutters@hjp-consulting.com>
2015-11-04 08:59:20 +0100

    moved methods to sub elements

--- de.persosim.simulator/src/de/persosim/simulator/perso/Personalization.java
+++ de.persosim.simulator/src/de/persosim/simulator/perso/Personalization.java
@@ -2,11 +2,8 @@ package de.persosim.simulator.perso;
 
 import java.util.List;
 
-import de.persosim.simulator.cardobjects.MasterFile;
-import de.persosim.simulator.platform.CardStateAccessor;
 import de.persosim.simulator.platform.CommandProcessor;
 import de.persosim.simulator.platform.Layer;
-import de.persosim.simulator.protocols.Protocol;
 
 /**
  * This interface describes all essential aspects of a simulated SmartCard
@@ -21,26 +18,12 @@ import de.persosim.simulator.protocols.Protocol;
 public interface Personalization {
 
 	/**
-	 * Returns the root element of the object tree.
-	 */
-	MasterFile getObjectTree();
-
-	/**
-	 * Returns the list of activated protocols.
-	 * <p/>
-	 * The protocols contained in this List are required to be already
-	 * initialized and ready to be added to a {@link CardStateAccessor} and
-	 * used afterwards
-	 * 
-	 * @return
-	 */
-	List<Protocol> getProtocolList();
-	
-	/**
 	 * Returns the list of layers to be used.
 	 * 
 	 * @return
 	 */
 	List<Layer> getLayerList();
 
+	public void initialize();
+	
 }
