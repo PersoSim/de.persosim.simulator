e4d9041c750685ac01f890211a5d3c957348c7c5
Julian Koch <julian.koch@hjp-consulting.com>
2015-11-24 13:48:40 +0100

    Added Javadoc to SessionContextIdMechanism

--- de.persosim.simulator/src/de/persosim/simulator/secstatus/SessionContextIdMechanism.java
+++ de.persosim.simulator/src/de/persosim/simulator/secstatus/SessionContextIdMechanism.java
@@ -1,5 +1,15 @@
 package de.persosim.simulator.secstatus;
 
+/**
+ * This Mechanism contains the Identifier that should be used to store the
+ * session context. The ID for the Default Context (ID=0) is set at the end of
+ * the PACE protocol. Other IDs are provided during MSE: set AT commands in tag
+ * E0. The storing of the default context happens before the CA initializes new
+ * Secure Messaging. Storing of other contexts happens before restoring old ones.
+ * 
+ * @author jkoch
+ *
+ */
 public class SessionContextIdMechanism extends AbstractSecMechanism{
 	
 	private int sessionContextId;
