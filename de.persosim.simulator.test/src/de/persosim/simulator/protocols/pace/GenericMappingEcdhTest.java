f23401d5d33378fb78868960d3cb922b631b6f3e
Sascha Lutters <sascha@globaltester.org>
2016-09-15 15:12:41 +0200

    fixed indentation

--- de.persosim.simulator.test/src/de/persosim/simulator/protocols/pace/GenericMappingEcdhTest.java
+++ de.persosim.simulator.test/src/de/persosim/simulator/protocols/pace/GenericMappingEcdhTest.java
@@ -33,50 +33,50 @@ import de.persosim.simulator.utils.HexString;
 public class GenericMappingEcdhTest extends PersoSimTestCase {
 	
 	// ---> ECDH <---
-		ECPoint ecdhGeneratorUnmappedExpected, ecdhGeneratorMappedExpected, ecdhPublicKeyPointUnmappedExpected, ecdhPublicKeyPointMappedExpected;
-		BigInteger ecdhPrivateKeySExpected;
-		DomainParameterSetEcdh ecdhDomainParameterSetUnMappedExpected, ecdhDomainParameterSetMappedExpected;
-		ECPrivateKey ecdhPrivateKeyUnmappedExpected, ecdhPrivateKeyMappedExpected;
-		ECPublicKey ecdhPublicKeyUnmappedExpected, ecdhPublicKeyMappedExpected;
-		KeyPair ecdhKeyPairUnmappedExpected, ecdhKeyPairMappedExpected;
+	ECPoint ecdhGeneratorUnmappedExpected, ecdhGeneratorMappedExpected, ecdhPublicKeyPointUnmappedExpected, ecdhPublicKeyPointMappedExpected;
+	BigInteger ecdhPrivateKeySExpected;
+	DomainParameterSetEcdh ecdhDomainParameterSetUnMappedExpected, ecdhDomainParameterSetMappedExpected;
+	ECPrivateKey ecdhPrivateKeyUnmappedExpected, ecdhPrivateKeyMappedExpected;
+	ECPublicKey ecdhPublicKeyUnmappedExpected, ecdhPublicKeyMappedExpected;
+	KeyPair ecdhKeyPairUnmappedExpected, ecdhKeyPairMappedExpected;
+	
+	/**
+	 * Create the test environment containing an elementary file and the mocked
+	 * object store.
+	 * @throws ReflectiveOperationException 
+	 */
+	@Before
+	public void setUp() throws Exception {
+		// this setup is currently used only within testPerformMapping, maybe move it there
+		ecdhDomainParameterSetUnMappedExpected = (DomainParameterSetEcdh) StandardizedDomainParameters.getDomainParameterSetById(13);
+		ecdhGeneratorUnmappedExpected = ecdhDomainParameterSetUnMappedExpected.getGenerator();
 		
-		/**
-		 * Create the test environment containing an elementary file and the mocked
-		 * object store.
-		 * @throws ReflectiveOperationException 
-		 */
-		@Before
-		public void setUp() throws Exception {
-			// this setup is currently used only within testPerformMapping, maybe move it there
-			ecdhDomainParameterSetUnMappedExpected = (DomainParameterSetEcdh) StandardizedDomainParameters.getDomainParameterSetById(13);
-			ecdhGeneratorUnmappedExpected = ecdhDomainParameterSetUnMappedExpected.getGenerator();
-			
-			byte[] ecdhGeneratorXMappedPlain = HexString.toByteArray("3D4C109FAF4EFD9FA4BDA13907401379067BF7F03C3ACE6D980575F6F9179E97");
-			byte[] ecdhGeneratorYMappedPlain = HexString.toByteArray("61E6418377A06DD48D4B624ABC54F7DAA54008E445E8B808713E406726106E2E");
-			ecdhGeneratorMappedExpected = new ECPoint(new BigInteger(1, ecdhGeneratorXMappedPlain), new BigInteger(1, ecdhGeneratorYMappedPlain));
-			
-			ecdhDomainParameterSetMappedExpected = ecdhDomainParameterSetUnMappedExpected.getUpdatedDomainParameterSet(ecdhGeneratorMappedExpected);
-			
-			byte[] ecdhPrivateSPlain = HexString.toByteArray("74F6631E3C1A185918CDE69B80016B5F6C34A686BD4E1DC2470911C4A6280A92");
-			ecdhPrivateKeySExpected = new BigInteger(1, ecdhPrivateSPlain);
-			
-			byte[] ecdhPublicXUnmappedPlain = HexString.toByteArray("16CD374C07A5A00354F5AACC22DC0C5A57AF154E29F3DFDD17A3F05E4374DEF0");
-			byte[] ecdhPublicYUnmappedPlain = HexString.toByteArray("A6478E512FE183B553EF84A424B0949A913953AC498FF7C4E316F1C71D9BD62A");
-			ecdhPublicKeyPointUnmappedExpected = new ECPoint(new BigInteger(1, ecdhPublicXUnmappedPlain), new BigInteger(1, ecdhPublicYUnmappedPlain));
-			
-			ECPrivateKeySpec ecdhPrivateKeySpecUnmappedExpected = new ECPrivateKeySpec(ecdhPrivateKeySExpected, ecdhDomainParameterSetUnMappedExpected.getKeySpec());
-			ECPublicKeySpec ecdhPublicKeySpecUnmappedExpected = new ECPublicKeySpec(ecdhPublicKeyPointUnmappedExpected, ecdhDomainParameterSetUnMappedExpected.getKeySpec());
-			
-			KeyFactory keyFactory = KeyFactory.getInstance("ECDH", Crypto.getCryptoProvider());
-			ecdhPrivateKeyUnmappedExpected = (ECPrivateKey) keyFactory.generatePrivate(ecdhPrivateKeySpecUnmappedExpected);
-			ecdhPublicKeyUnmappedExpected = (ECPublicKey) keyFactory.generatePublic(ecdhPublicKeySpecUnmappedExpected);
-			
-			ecdhKeyPairUnmappedExpected = new KeyPair(ecdhPublicKeyUnmappedExpected, ecdhPrivateKeyUnmappedExpected);
-			ecdhKeyPairMappedExpected = CryptoUtil.updateKeyPairToNewDomainParameters(ecdhKeyPairUnmappedExpected, ecdhDomainParameterSetMappedExpected);
-			
-			ecdhPrivateKeyMappedExpected = (ECPrivateKey) ecdhKeyPairMappedExpected.getPrivate();
-			ecdhPublicKeyMappedExpected = (ECPublicKey) ecdhKeyPairMappedExpected.getPublic();
-		}
+		byte[] ecdhGeneratorXMappedPlain = HexString.toByteArray("3D4C109FAF4EFD9FA4BDA13907401379067BF7F03C3ACE6D980575F6F9179E97");
+		byte[] ecdhGeneratorYMappedPlain = HexString.toByteArray("61E6418377A06DD48D4B624ABC54F7DAA54008E445E8B808713E406726106E2E");
+		ecdhGeneratorMappedExpected = new ECPoint(new BigInteger(1, ecdhGeneratorXMappedPlain), new BigInteger(1, ecdhGeneratorYMappedPlain));
+		
+		ecdhDomainParameterSetMappedExpected = ecdhDomainParameterSetUnMappedExpected.getUpdatedDomainParameterSet(ecdhGeneratorMappedExpected);
+		
+		byte[] ecdhPrivateSPlain = HexString.toByteArray("74F6631E3C1A185918CDE69B80016B5F6C34A686BD4E1DC2470911C4A6280A92");
+		ecdhPrivateKeySExpected = new BigInteger(1, ecdhPrivateSPlain);
+		
+		byte[] ecdhPublicXUnmappedPlain = HexString.toByteArray("16CD374C07A5A00354F5AACC22DC0C5A57AF154E29F3DFDD17A3F05E4374DEF0");
+		byte[] ecdhPublicYUnmappedPlain = HexString.toByteArray("A6478E512FE183B553EF84A424B0949A913953AC498FF7C4E316F1C71D9BD62A");
+		ecdhPublicKeyPointUnmappedExpected = new ECPoint(new BigInteger(1, ecdhPublicXUnmappedPlain), new BigInteger(1, ecdhPublicYUnmappedPlain));
+		
+		ECPrivateKeySpec ecdhPrivateKeySpecUnmappedExpected = new ECPrivateKeySpec(ecdhPrivateKeySExpected, ecdhDomainParameterSetUnMappedExpected.getKeySpec());
+		ECPublicKeySpec ecdhPublicKeySpecUnmappedExpected = new ECPublicKeySpec(ecdhPublicKeyPointUnmappedExpected, ecdhDomainParameterSetUnMappedExpected.getKeySpec());
+		
+		KeyFactory keyFactory = KeyFactory.getInstance("ECDH", Crypto.getCryptoProvider());
+		ecdhPrivateKeyUnmappedExpected = (ECPrivateKey) keyFactory.generatePrivate(ecdhPrivateKeySpecUnmappedExpected);
+		ecdhPublicKeyUnmappedExpected = (ECPublicKey) keyFactory.generatePublic(ecdhPublicKeySpecUnmappedExpected);
+		
+		ecdhKeyPairUnmappedExpected = new KeyPair(ecdhPublicKeyUnmappedExpected, ecdhPrivateKeyUnmappedExpected);
+		ecdhKeyPairMappedExpected = CryptoUtil.updateKeyPairToNewDomainParameters(ecdhKeyPairUnmappedExpected, ecdhDomainParameterSetMappedExpected);
+		
+		ecdhPrivateKeyMappedExpected = (ECPrivateKey) ecdhKeyPairMappedExpected.getPrivate();
+		ecdhPublicKeyMappedExpected = (ECPublicKey) ecdhKeyPairMappedExpected.getPublic();
+	}
 	
 	/**
 	 * Positive test case: perform mapping of ECDH domain parameters based on values from valid PACE test run.
