a296d7fe42b0c6ac189442c55e0a5d37611f8626
Martin Boonk <martin.boonk@secunet.com>
2016-10-25 10:44:34 +0200

    Moved the checkFilesForDifferences method to o.g.b.utils.Utils class

--- de.persosim.simulator.test/src/de/persosim/simulator/perso/ArtifactPersonalizationTest.java
+++ de.persosim.simulator.test/src/de/persosim/simulator/perso/ArtifactPersonalizationTest.java
@@ -4,11 +4,10 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Paths;
 import java.util.LinkedList;
-import java.util.List;
+
+import org.globaltester.base.utils.Utils;
 
 import de.persosim.simulator.exception.AccessDeniedException;
 
@@ -34,67 +33,9 @@ public abstract class ArtifactPersonalizationTest extends PersonalizationTest {
 		LinkedList<Byte> currentContext = new LinkedList<Byte>();
 		LinkedList<Byte> previousContext = new LinkedList<Byte>();
 		
-		long positionInFile = checkFilesForDifferences(previousFile, currentlyMarshalledFile, contextSize, previousContext, currentContext);
+		long positionInFile = Utils.checkFilesForDifferences(new FileInputStream(previousFile), new FileInputStream(currentlyMarshalledFile), contextSize, previousContext, currentContext);
 
-		assertEquals("Found difference at byte " + positionInFile + ", " + contextSize + " bytes context provided.", toString(previousContext), toString(currentContext));
-	}
-	
-	/**
-	 * Method for checking files for differences at byte level.
-	 * @param expected the {@link File} which contains the expected content
-	 * @param fileToCheck the {@link File} which should be checked for differences
-	 * @param contextSize the number of bytes to store as context before differences
-	 * @param expectedContext context buffer for the expected file
-	 * @param fileToCheckContext context buffer for the file to be checked
-	 * @return the position of the first differing byte or -1 if no differences found
-	 * @throws IOException
-	 */
-	public static long checkFilesForDifferences(File expected, File fileToCheck, int contextSize, LinkedList<Byte> expectedContext,
-			LinkedList<Byte> fileToCheckContext) throws IOException {
-		try (InputStreamReader currentlyMarshalledFileInputStreamReader = new InputStreamReader(
-				new FileInputStream(fileToCheck));
-				InputStreamReader previousFileInputStreamReader = new InputStreamReader(
-						new FileInputStream(expected));) {
-
-			int fileToCheckReadByte = 0;
-			int expectedReadByte = 0;
-			long positionInFile = 0;
-
-			do {
-				fileToCheckReadByte = currentlyMarshalledFileInputStreamReader.read();
-				expectedReadByte = previousFileInputStreamReader.read();
-
-				fileToCheckContext.add((byte) fileToCheckReadByte);
-				expectedContext.add((byte) expectedReadByte);
-				if (fileToCheckContext.size() >= contextSize) {
-					fileToCheckContext.remove(0);
-				}
-				if (expectedContext.size() >= contextSize) {
-					expectedContext.remove(0);
-				}
-
-				if (fileToCheckReadByte != expectedReadByte) {
-					return positionInFile;
-				}
-				positionInFile++;
-			} while (fileToCheckReadByte != -1);
-
-		}
-
-		return -1;
-	}
-
-	/**
-	 * Converts a list of bytes to a string using UTF-8
-	 * @param list
-	 * @return
-	 */
-	private String toString(List<Byte> list) {
-		StringBuilder builder = new StringBuilder();
-		for (Byte b : list){
-			builder.append((char)(byte)b);
-		}
-		return builder.toString();
+		assertEquals("Found difference at byte " + positionInFile + ", " + contextSize + " bytes context provided.", Utils.toString(previousContext), Utils.toString(currentContext));
 	}
 
 	/**
