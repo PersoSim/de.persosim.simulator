b5e45e93f4279f916264d88f239240e2e8db1d83
Sascha Lutters <sascha.lutters@secunet.com>
2016-11-04 12:14:00 +0100

    added unit tests

--- de.persosim.simulator.test/src/de/persosim/simulator/tlv/Asn1DataStructuresTest.java
+++ de.persosim.simulator.test/src/de/persosim/simulator/tlv/Asn1DataStructuresTest.java
@@ -9,10 +9,21 @@ import de.persosim.simulator.utils.HexString;
 public class Asn1DataStructuresTest {
 	
 	/**
-	 * Positive test: test generation of TLV structure for data group containing ASN.1 data type Date.
+	 * Positive test: test generation of ASN.1 data structure "Date".
 	 */
 	@Test
-	public void testGetDateDgTlv() {
+	public void testAsn1DateEncode() {
+		PrimitiveTlvDataObject received = Asn1Date.getInstance().encode("20201031");
+		PrimitiveTlvDataObject expected = new PrimitiveTlvDataObject(HexString.toByteArray("12083230323031303331"));
+		
+		assertArrayEquals(expected.toByteArray(), received.toByteArray());
+	}
+	
+	/**
+	 * Positive test: test generation of wrapped ASN.1 data structure "Date".
+	 */
+	@Test
+	public void testAsn1DateWrapperEncode() {
 		ConstructedTlvDataObject received = Asn1DateWrapper.getInstance().encode(new TlvTag((byte) 0x63), "20201031");
 		ConstructedTlvDataObject expected = new ConstructedTlvDataObject(HexString.toByteArray("630A12083230323031303331"));
 		
@@ -20,21 +31,21 @@ public class Asn1DataStructuresTest {
 	}
 	
 	/**
-	 * Positive test: test generation of TLV structure for data group containing ASN.1 data type ICAOString.
+	 * Positive test: test generation of ASN.1 data structure "ICAOCountry".
 	 */
 	@Test
-	public void testGetIcaoStringDgTlv() {
-		ConstructedTlvDataObject received = Asn1IcaoStringWrapper.getInstance().encode(new TlvTag((byte) 0x61), "ID");
-		ConstructedTlvDataObject expected = new ConstructedTlvDataObject(HexString.toByteArray("610413024944"));
+	public void testAsn1IcaoCountryEncode() {
+		PrimitiveTlvDataObject received = Asn1IcaoCountry.getInstance().encode("D");
+		PrimitiveTlvDataObject expected = new PrimitiveTlvDataObject(HexString.toByteArray("130144"));
 		
 		assertArrayEquals(expected.toByteArray(), received.toByteArray());
 	}
 	
 	/**
-	 * Positive test: test generation of TLV structure for data group containing ASN.1 data type IssuingState.
+	 * Positive test: test generation of wrapped ASN.1 data structure "ICAOCountry".
 	 */
 	@Test
-	public void testGetIssuingStateDgTlv() {
+	public void testAsn1IcaoCountryWrapperEncode() {
 		ConstructedTlvDataObject received = Asn1IcaoCountryWrapper.getInstance().encode(new TlvTag((byte) 0x62), "D");
 		ConstructedTlvDataObject expected = new ConstructedTlvDataObject(HexString.toByteArray("6203130144"));
 		
@@ -42,10 +53,43 @@ public class Asn1DataStructuresTest {
 	}
 	
 	/**
-	 * Positive test: test generation of TLV structure for data group containing ASN.1 data type UTF8String.
+	 * Positive test: test generation of ASN.1 data structure "ICAOString".
 	 */
 	@Test
-	public void testGetUtf8StringDgTlv() {
+	public void testAsn1IcaoStringEncode() {
+		PrimitiveTlvDataObject received = Asn1IcaoString.getInstance().encode("ID");
+		PrimitiveTlvDataObject expected = new PrimitiveTlvDataObject(HexString.toByteArray("13024944"));
+		
+		assertArrayEquals(expected.toByteArray(), received.toByteArray());
+	}
+	
+	/**
+	 * Positive test: test generation of wrapped ASN.1 data structure "ICAOString".
+	 */
+	@Test
+	public void testAsn1IcaoStringWrapperEncode() {
+		ConstructedTlvDataObject received = Asn1IcaoStringWrapper.getInstance().encode(new TlvTag((byte) 0x61), "ID");
+		ConstructedTlvDataObject expected = new ConstructedTlvDataObject(HexString.toByteArray("610413024944"));
+		
+		assertArrayEquals(expected.toByteArray(), received.toByteArray());
+	}
+	
+	/**
+	 * Positive test: test generation of ASN.1 data structure "UTF8String".
+	 */
+	@Test
+	public void testAsn1Utf8StringEncode() {
+		PrimitiveTlvDataObject received = Asn1Utf8String.getInstance().encode("ERIKA");
+		PrimitiveTlvDataObject expected = new PrimitiveTlvDataObject(HexString.toByteArray("0C054552494B41"));
+		
+		assertArrayEquals(expected.toByteArray(), received.toByteArray());
+	}
+	
+	/**
+	 * Positive test: test generation of wrapped ASN.1 data structure "UTF8String".
+	 */
+	@Test
+	public void testAsn1Utf8StringWrapperEncode() {
 		ConstructedTlvDataObject received = Asn1Utf8StringWrapper.getInstance().encode(new TlvTag((byte) 0x64), "ERIKA");
 		ConstructedTlvDataObject expected = new ConstructedTlvDataObject(HexString.toByteArray("64070C054552494B41"));
 		
