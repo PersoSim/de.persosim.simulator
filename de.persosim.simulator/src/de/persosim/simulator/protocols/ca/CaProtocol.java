22dbf11a256c7d8e6b92f975d9308ca61e1f401c
Sascha Lutters <sascha.lutters@secunet.com>
2017-02-16 09:56:42 +0100

    added methods for setting state machine state variables

--- de.persosim.simulator/src/de/persosim/simulator/protocols/ca/CaProtocol.java
+++ de.persosim.simulator/src/de/persosim/simulator/protocols/ca/CaProtocol.java
@@ -1,5 +1,13 @@
 package de.persosim.simulator.protocols.ca;
 
 public class CaProtocol extends DefaultCaProtocol {
-
+	
+	public void setStateVar(int value) {
+		stateVar = value;
+	}
+	
+	public void setStateVarCA_IN_PROGRESS(int value) {
+		stateVarCA_IN_PROGRESS = value;
+	}
+	
 }
