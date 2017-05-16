0ae4479fe61352aae46cdf2a8764b963c0177d9d
Sascha Lutters <sascha.lutters@secunet.com>
2017-05-16 12:00:49 +0200

    added tests to newly imported class

---
+++ de.persosim.simulator.test/src/de/persosim/simulator/crypto/Tr03111UtilsTest.java
@@ -0,0 +1,55 @@
+package de.persosim.simulator.crypto;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.math.BigInteger;
+
+import org.junit.Test;
+
+import de.persosim.simulator.crypto.Tr03111Utils;
+
+import de.persosim.simulator.protocols.Tr03110Utils;
+import de.persosim.simulator.test.PersoSimTestCase;
+
+public class Tr03111UtilsTest extends PersoSimTestCase {	
+	/**
+	 * Negative test: test {@link Tr03110Utils#i2os(BigInteger, int)} for a negative BigInteger.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testI2os_NegativeBigInt() {
+		BigInteger bigInt = new BigInteger("-42");
+		Tr03111Utils.i2os(bigInt, 42);
+	}
+	
+	/**
+	 * Negative test: test {@link Tr03110Utils#i2os(BigInteger, int)} for a provided length smaller than the length of the actual octet String.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testI2os_lengthSmallerThanOctetString() {
+		BigInteger bigInt = new BigInteger("42");
+		Tr03111Utils.i2os(bigInt, 0);
+	}
+	
+	/**
+	 * Positive test: test {@link Tr03110Utils#i2os(BigInteger, int)} for a provided length matching the length of the actual octet String.
+	 */
+	@Test
+	public void testI2os_lengthMatchingOctetString() {
+		BigInteger bigInt = new BigInteger("42");
+		byte[] expectedResult = new byte[] {(byte) 42};
+		byte[] result = Tr03111Utils.i2os(bigInt, expectedResult.length);
+		assertArrayEquals(expectedResult, result);
+	}
+	
+	/**
+	 * Positive test: test {@link Tr03110Utils#i2os(BigInteger, int)} for a provided length longer than the length of the actual octet String.
+	 */
+	@Test
+	public void testI2os_lengthLongerThanOctetString() {
+		BigInteger bigInt = new BigInteger("42");
+		byte[] expectedResult = new byte[] {(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 42};
+		byte[] result = Tr03111Utils.i2os(bigInt, expectedResult.length);
+		assertArrayEquals(expectedResult, result);
+	}
+	
+}
