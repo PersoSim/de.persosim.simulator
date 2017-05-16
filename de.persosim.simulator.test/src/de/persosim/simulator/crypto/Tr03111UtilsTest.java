70aefd1b324797804653980cf6927dfc9aaab169
Sascha Lutters <sascha.lutters@secunet.com>
2017-05-16 12:26:20 +0200

    renamed tests, added tests for i2os(byte[], int)

--- de.persosim.simulator.test/src/de/persosim/simulator/crypto/Tr03111UtilsTest.java
+++ de.persosim.simulator.test/src/de/persosim/simulator/crypto/Tr03111UtilsTest.java
@@ -11,12 +11,13 @@ import de.persosim.simulator.crypto.Tr03111Utils;
 import de.persosim.simulator.protocols.Tr03110Utils;
 import de.persosim.simulator.test.PersoSimTestCase;
 
-public class Tr03111UtilsTest extends PersoSimTestCase {	
+public class Tr03111UtilsTest extends PersoSimTestCase {
+	
 	/**
 	 * Negative test: test {@link Tr03110Utils#i2os(BigInteger, int)} for a negative BigInteger.
 	 */
 	@Test(expected = IllegalArgumentException.class)
-	public void testI2os_NegativeBigInt() {
+	public void testI2os_BigInt_NegativeBigInt() {
 		BigInteger bigInt = new BigInteger("-42");
 		Tr03111Utils.i2os(bigInt, 42);
 	}
@@ -25,7 +26,7 @@ public class Tr03111UtilsTest extends PersoSimTestCase {
 	 * Negative test: test {@link Tr03110Utils#i2os(BigInteger, int)} for a provided length smaller than the length of the actual octet String.
 	 */
 	@Test(expected = IllegalArgumentException.class)
-	public void testI2os_lengthSmallerThanOctetString() {
+	public void testI2os_BigInt_lengthSmallerThanOctetString() {
 		BigInteger bigInt = new BigInteger("42");
 		Tr03111Utils.i2os(bigInt, 0);
 	}
@@ -34,7 +35,7 @@ public class Tr03111UtilsTest extends PersoSimTestCase {
 	 * Positive test: test {@link Tr03110Utils#i2os(BigInteger, int)} for a provided length matching the length of the actual octet String.
 	 */
 	@Test
-	public void testI2os_lengthMatchingOctetString() {
+	public void testI2os_BigInt_lengthMatchingOctetString() {
 		BigInteger bigInt = new BigInteger("42");
 		byte[] expectedResult = new byte[] {(byte) 42};
 		byte[] result = Tr03111Utils.i2os(bigInt, expectedResult.length);
@@ -45,8 +46,41 @@ public class Tr03111UtilsTest extends PersoSimTestCase {
 	 * Positive test: test {@link Tr03110Utils#i2os(BigInteger, int)} for a provided length longer than the length of the actual octet String.
 	 */
 	@Test
-	public void testI2os_lengthLongerThanOctetString() {
+	public void testI2os_BigInt_lengthLongerThanOctetString() {
 		BigInteger bigInt = new BigInteger("42");
+		byte[] expectedResult = new byte[] {(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 42};
+		byte[] result = Tr03111Utils.i2os(bigInt, expectedResult.length);
+		assertArrayEquals(expectedResult, result);
+	}
+	
+	/**
+	 * Positive test: test {@link Tr03110Utils#i2os(byte[], int)} for a provided length shorter than the length of the actual octet String.
+	 */
+	@Test
+	public void testI2os_ByteArray_lengthShorterThanOctetString() {
+		byte[] bigInt = new byte[] {(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 42};
+		byte[] expectedResult = new byte[] {(byte) 0x00, (byte) 0x00, (byte) 42};
+		byte[] result = Tr03111Utils.i2os(bigInt, expectedResult.length);
+		assertArrayEquals(expectedResult, result);
+	}
+	
+	/**
+	 * Positive test: test {@link Tr03110Utils#i2os(byte[], int)} for a provided length longer than the length of the actual octet String.
+	 */
+	@Test
+	public void testI2os_ByteArray_lengthLongerThanOctetString() {
+		byte[] bigInt = new byte[] {(byte) 0x00, (byte) 0x00, (byte) 42};
+		byte[] expectedResult = new byte[] {(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 42};
+		byte[] result = Tr03111Utils.i2os(bigInt, expectedResult.length);
+		assertArrayEquals(expectedResult, result);
+	}
+	
+	/**
+	 * Positive test: test {@link Tr03110Utils#i2os(byte[], int)} for a provided lengthmatching the length of the actual octet String.
+	 */
+	@Test
+	public void testI2os_ByteArray_lengthMatchingOctetString() {
+		byte[] bigInt = new byte[] {(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 42};
 		byte[] expectedResult = new byte[] {(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 42};
 		byte[] result = Tr03111Utils.i2os(bigInt, expectedResult.length);
 		assertArrayEquals(expectedResult, result);
