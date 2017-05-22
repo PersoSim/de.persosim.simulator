e2df9fd57cb8fe58f899627acb07d058d306e859
Sascha Lutters <sascha.lutters@secunet.com>
2017-05-22 11:28:02 +0200

    added MrzFactory for generically parsing MRZs

---
+++ de.persosim.simulator/src/de/persosim/simulator/documents/MrzFactory.java
@@ -0,0 +1,23 @@
+package de.persosim.simulator.documents;
+
+/**
+ * 
+ */
+public class MrzFactory {
+	
+	private MrzFactory() {};
+	
+	public static Mrz parseMrz(String mrzString) {
+		
+		int length = mrzString.length();
+		
+		switch (length) {
+			case 90: return new MrzTD1(mrzString);
+			case 72: return new MrzTD2(mrzString);
+			case 88: return new MrzTD3(mrzString);
+			default: throw new IllegalArgumentException("Unsupported length of MRZ");
+		}
+		
+	}
+	
+}
