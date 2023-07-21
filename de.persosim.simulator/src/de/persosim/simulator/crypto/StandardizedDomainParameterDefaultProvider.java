693c707a59780e0d5ea082fff636b149e566bc22
Stefan Philipp <stefan.philipp@secunet.com>
2023-07-21 16:33:16 +0200

    change private to public

--- de.persosim.simulator/src/de/persosim/simulator/crypto/StandardizedDomainParameterDefaultProvider.java
+++ de.persosim.simulator/src/de/persosim/simulator/crypto/StandardizedDomainParameterDefaultProvider.java
@@ -185,7 +185,7 @@ public class StandardizedDomainParameterDefaultProvider implements
 	 * @param b coefficient B defining the curve
 	 * @return the corresponding {@link EllipticCurve} object
 	 */
-	private static EllipticCurve generateCurveFrom(BigInteger p, BigInteger a, BigInteger b) {
+	public static EllipticCurve generateCurveFrom(BigInteger p, BigInteger a, BigInteger b) {
 		return new EllipticCurve(new ECFieldFp(p), a, b);
 	}	
 
