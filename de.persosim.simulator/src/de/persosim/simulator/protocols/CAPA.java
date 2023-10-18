0c8ad206c5885eb8a9f2c8130dc19ce5141ff3d8
Stefan Philipp <stefan.philipp@secunet.com>
2023-10-18 10:13:32 +0200

    OID added

--- de.persosim.simulator/src/de/persosim/simulator/protocols/CAPA.java
+++ de.persosim.simulator/src/de/persosim/simulator/protocols/CAPA.java
@@ -9,7 +9,7 @@ import de.persosim.simulator.utils.HexString;
 public interface CAPA extends Tr03110 {
 
 	public static final String PROTOCOL_NAME = "CA+PA";
-	
+
 	public static final Oid id_protocols = new GenericOid(id_BSI, HexString.toByteArray("02"));
 	public static final Oid id_smartcard = new GenericOid(id_protocols, HexString.toByteArray("02"));
 
@@ -20,6 +20,7 @@ public interface CAPA extends Tr03110 {
 	public static final Oid id_CAPA_ECDH_AES_CBC_CMAC_192 = new GenericOid(id_CAPA_ECDH, HexString.toByteArray("03"));
 	public static final Oid id_CAPA_ECDH_AES_CBC_CMAC_256 = new GenericOid(id_CAPA_ECDH, HexString.toByteArray("04"));
 
+	public static final OidIdentifier OID_IDENTIFIER_id_CAPA_ECDH = new OidIdentifier(id_CAPA_ECDH);
 	public static final OidIdentifier OID_IDENTIFIER_id_CAPA_ECDH_AES_CBC_CMAC_128 = new OidIdentifier(
 			id_CAPA_ECDH_AES_CBC_CMAC_128);
 	public static final OidIdentifier OID_IDENTIFIER_id_CAPA_ECDH_AES_CBC_CMAC_192 = new OidIdentifier(
