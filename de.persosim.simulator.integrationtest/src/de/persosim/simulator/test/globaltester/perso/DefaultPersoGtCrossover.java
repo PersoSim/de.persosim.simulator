6ec331f9bc8c74fee53f69cd1188bab2af1b89d8
Stefan Philipp <stefan.philipp@secunet.com>
2025-01-30 13:07:00 +0100

    Removed duplicated sampleConfig entry CAPA

--- de.persosim.simulator.integrationtest/src/de/persosim/simulator/test/globaltester/perso/DefaultPersoGtCrossover.java
+++ de.persosim.simulator.integrationtest/src/de/persosim/simulator/test/globaltester/perso/DefaultPersoGtCrossover.java
@@ -27,21 +27,21 @@ import de.persosim.simulator.perso.DefaultPersoGt;
 import de.persosim.simulator.perso.Personalization;
 
 public class DefaultPersoGtCrossover extends DefaultScriptIntegrationTest{
-	
+
 	public static final String isRootFolderName = "certificates/cv/is";
 	public static final String atRootFolderName = "certificates/cv/at";
 	public static final String stRootFolderName = "certificates/cv/st";
-	
+
 	protected static final String eac2Certs = com.secunet.globaltester.protocols.eac2.certificates.EacCertificatesFactory.PROTOCOL_NAME;
-	
+
 
 	@Override
 	public IProject getSampleConfigChip() throws Exception {
 		IProject sampleConfigProject = super.getSampleConfigChip();
-		SampleConfig sampleConfig = SampleConfig.getSampleConfigForProject(sampleConfigProject);				
+		SampleConfig sampleConfig = SampleConfig.getSampleConfigForProject(sampleConfigProject);
 		//remove unsupported profiles
         modifySampleConfigForPerso(sampleConfig);
-		
+
 		//copy root certificates/keys
 		GtResourceHelper.copyPluginFilesToWorkspaceProject("de.persosim.simulator", sampleConfig.getProject() .getFolder(isRootFolderName),
 				"personalization/gtCertificates/CFG.DFLT.EAC.IS", Collections.emptyList());
@@ -52,16 +52,16 @@ public class DefaultPersoGtCrossover extends DefaultScriptIntegrationTest{
 
 
 		sampleConfig.saveToProject();
-		
+
 		return sampleConfigProject;
 	}
-	
+
 	protected void modifySampleConfigForPerso(SampleConfig sampleConfig)
 			throws IOException {
 		sampleConfig.setHaveToSaveToProjectAfterPut(false);
 		sampleConfig.put("IEA", "IEA", "false");
 		sampleConfig.put("MOBILEID", "MOBILEID", "false");
-		
+
 		sampleConfig.put("eID", "CS", "false");
 	    sampleConfig.put("eID", "DG16", "false");
 	    sampleConfig.put("eID", "DG15", "false");
@@ -76,34 +76,32 @@ public class DefaultPersoGtCrossover extends DefaultScriptIntegrationTest{
 
 	    sampleConfig.put("TAv2", "RSA", "false");
 	    sampleConfig.put("TAv2", "AUTH_EXT", "false");
-	    
+
 	    sampleConfig.put("CAv3", "CA", "false");
-	    
+
 	    sampleConfig.put("eSIGN", "eSIGN", "false");
-        
+
 	    sampleConfig.put("ISO7816", "OddIns", "false");
 	    sampleConfig.put("PASSWORDS", "CNG_CAN_AR", "false");
-	    
+
 	    sampleConfig.put("RI", "KEY_ID", "0x02");
 	    sampleConfig.put("RI", "KEY_ID_AUTH", "true");
-	    
+
 	    sampleConfig.put("TR03110", "PSAInfo", "false");
-		
+
 
 	    sampleConfig.put("ISO7816", "EFATR", "false");
 	    sampleConfig.put("ISO7816", "ENV", "false");
 	    sampleConfig.put("AuxData", "CMP", "false");
-		
+
 		sampleConfig.put("CAv2", "DH", "false");
 		sampleConfig.put("PACE", "CAM", "false");
 		sampleConfig.put("PACE", "DH", "false");
 	    sampleConfig.put("PACE", "IM", "false");
 	    sampleConfig.put("PACE", "STATIC_BINDING", "false");
-	    
-	    sampleConfig.put("CAPA", "CAPA", "false");
 
 	    sampleConfig.put("CAPA", "CAPA", "false");
-	    
+
 	    sampleConfig.put("PASSWORDS", "MRZ",
 				"IDD<<0000000011<<<<<<<<<<<<<<<\n"+
 				"8408129F3406304D<<<<<<<<<<<<<4\n"+
@@ -114,12 +112,12 @@ public class DefaultPersoGtCrossover extends DefaultScriptIntegrationTest{
 		sampleConfig.put(eac2Certificates, "USE_CERTS", "USE_GENERATED_CERTS");
 		sampleConfig.put(eac2Certificates, "EAC2_CERTS", "certificates/generated");
 		Files.createDirectories(Paths.get(sampleConfig.getAbsolutePath(eac2Certificates, "EAC2_CERTS")));
-		
+
 		IFolder isRootFolder = sampleConfig.getProject() .getFolder(isRootFolderName);
 		Files.createDirectories(Paths.get(isRootFolder.getRawLocationURI()));
 		sampleConfig.put(com.secunet.globaltester.protocols.eac2.certificates.EacCertificatesFactory.PROTOCOL_NAME, "IS_CVCA_CERT", isRootFolderName + "/CVCA_Cert_01.cvcert");
 		sampleConfig.put(com.secunet.globaltester.protocols.eac2.certificates.EacCertificatesFactory.PROTOCOL_NAME, "IS_CVCA_KEY", isRootFolderName + "/CVCA_KEY_01.pkcs8");
-		
+
 		IFolder atRootFolder = sampleConfig.getProject().getFolder(atRootFolderName);
 		Files.createDirectories(Paths.get(atRootFolder.getRawLocationURI()));
 		sampleConfig.put(com.secunet.globaltester.protocols.eac2.certificates.EacCertificatesFactory.PROTOCOL_NAME, "AT_CVCA_CERT", atRootFolderName + "/CVCA_Cert_01.cvcert");
@@ -129,7 +127,7 @@ public class DefaultPersoGtCrossover extends DefaultScriptIntegrationTest{
 		Files.createDirectories(Paths.get(stRootFolder.getRawLocationURI()));
 		sampleConfig.put(com.secunet.globaltester.protocols.eac2.certificates.EacCertificatesFactory.PROTOCOL_NAME, "ST_CVCA_CERT", stRootFolderName + "/CVCA_Cert_01.cvcert");
 		sampleConfig.put(com.secunet.globaltester.protocols.eac2.certificates.EacCertificatesFactory.PROTOCOL_NAME, "ST_CVCA_KEY", stRootFolderName + "/CVCA_KEY_01.pkcs8");
-		
+
 		//copy root certificates/keys
 		GtResourceHelper.copyPluginFilesToWorkspaceProject("de.persosim.simulator", sampleConfig.getProject() .getFolder(isRootFolderName),
 				"personalization/gtCertificates/CFG.DFLT.EAC.IS", Collections.emptyList());
@@ -149,7 +147,7 @@ public class DefaultPersoGtCrossover extends DefaultScriptIntegrationTest{
 	public List<String> getTestCasesTerminal() {
 		return Collections.emptyList();
 	}
-	
+
 	@Override
 	public List<String> getTestCasesChip() {
 		ArrayList<String> testcasesChip = new ArrayList<>();
@@ -172,47 +170,47 @@ public class DefaultPersoGtCrossover extends DefaultScriptIntegrationTest{
 //		testcasesChip.add("GT Scripts BSI TR03105 Part 3.3/TestSuites/Layer6/testsuite_ISO7816_Q.gtsuite");
 //		testcasesChip.add("GT Scripts BSI TR03105 Part 3.3/TestSuites/Layer6/ISO7816_Q/EAC2_ISO7816_Q_20.gt");
 //		testcasesChip.add("GT Scripts BSI TR03105 Part 3.3/TestSuites/Layer6/testsuite_ISO7816_R.gtsuite");
-		
+
 		testcasesChip.add("GT Scripts BSI TR03105 Part 3.3/TestSuites/Layer7/testsuite_complete_standard_layer7.gtsuite");
 //		testcasesChip.add("GT Scripts BSI TR03105 Part 3.3/TestSuites/Layer7/testsuite_DATA_A.gtsuite");
 //		testcasesChip.add("GT Scripts BSI TR03105 Part 3.3/TestSuites/Layer7/testsuite_DATA_B.gtsuite");
 //		testcasesChip.add("GT Scripts BSI TR03105 Part 3.3/TestSuites/Layer7/testsuite_DATA_C.gtsuite");
 //		testcasesChip.add("GT Scripts BSI TR03105 Part 3.3/TestSuites/Layer7/testsuite_EIDDATA_B.gtsuite");
-		
+
 		return testcasesChip;
 	}
 
 	@Test
 	public void defaultTest() throws Exception {
-		Personalization perso = new DefaultPersoGt();		
-		
+		Personalization perso = new DefaultPersoGt();
+
 		enableSimulator(perso);
-		
+
 		performChipTest(getSampleConfigChip(), getTestCasesChip());
-		
+
 		TestResult chipResults = getChipTestResult();
-		
+
 		disableSimulator();
-		
+
 		assertEquals("Chip test result", 0, chipResults.overallResult);
-		
+
 		cleanupChipTest();
 	}
-	
+
 
 	@Override
 	public TestResult getChipTestResult() throws InterruptedException, ExecutionException, TimeoutException {
 		TestResult chipTestResult = super.getChipTestResult();
 
 		List<String> expectedWarningTestcases = Arrays.asList(
-				"EAC2_ISO7816_M_9", 
-				"EAC2_ISO7816_M_10", 
-				"EAC2_ISO7816_M_11", 
-				"EAC2_ISO7816_M_12", 
-				"EAC2_ISO7816_M_13", 
+				"EAC2_ISO7816_M_9",
+				"EAC2_ISO7816_M_10",
+				"EAC2_ISO7816_M_11",
+				"EAC2_ISO7816_M_12",
+				"EAC2_ISO7816_M_13",
 				"EAC2_ISO7816_M_14"
 				);
-		
+
 		System.out.println("Test case results:");
 		for (SubTestResult current : chipTestResult.subResults){
 			String curResString = current.resultString;
@@ -222,13 +220,13 @@ public class DefaultPersoGtCrossover extends DefaultScriptIntegrationTest{
 			if (curResString.contains("WARNING") && !expectedWarningTestcases.contains(current.testCaseId)) {
 				fail("Testcase "+current.testCaseId+" returned unexpected warning" );
 			}
-			
+
 			if (curResString.contains("PASSED") && expectedWarningTestcases.contains(current.testCaseId)) {
 				fail("Testcase "+current.testCaseId+" returned unexpected status, WARNING expected but was PASSED" );
 			}
-			
+
 		}
-		
+
 		return chipTestResult;
 	}
 
