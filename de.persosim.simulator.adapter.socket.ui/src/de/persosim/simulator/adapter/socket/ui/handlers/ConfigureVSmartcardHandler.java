61f761c886353b5a7ce113b3ed41a96274bb83a7
Martin Boonk <martin.boonk@secunet.com>
2025-04-14 16:18:29 +0200

    PS-10 - Do nothing to reader while configuration dialog is open

--- de.persosim.simulator.adapter.socket.ui/src/de/persosim/simulator/adapter/socket/ui/handlers/ConfigureVSmartcardHandler.java
+++ de.persosim.simulator.adapter.socket.ui/src/de/persosim/simulator/adapter/socket/ui/handlers/ConfigureVSmartcardHandler.java
@@ -6,8 +6,6 @@ import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.swt.widgets.Shell;
 
-import de.persosim.driver.connector.ui.parts.ReaderPart;
-import de.persosim.driver.connector.ui.parts.ReaderPart.ReaderType;
 import de.persosim.simulator.adapter.socket.ui.parts.ConfigVSmartcardDialog;
 import jakarta.inject.Inject;
 
@@ -20,24 +18,10 @@ public class ConfigureVSmartcardHandler {
 	public void execute(Shell shell) {
 		// ID of part as defined in fragment.e4xmi application model
 		MPart readerPart = partService.findPart("de.persosim.driver.connector.ui.parts.reader");
-		
-		//disable reader while configuring
-		if (readerPart.getObject() instanceof ReaderPart) {
-			ReaderPart readerPartObject = (ReaderPart) readerPart.getObject();
-			
-			readerPartObject.switchReaderType(ReaderType.NONE);
-		}
-		
+
 		//configuration dialog
 		ConfigVSmartcardDialog dialog = new ConfigVSmartcardDialog(shell, readerPart);
 		dialog.open();
-		
-		//restart reader
-		if (readerPart.getObject() instanceof ReaderPart) {
-			ReaderPart readerPartObject = (ReaderPart) readerPart.getObject();
-			
-			readerPartObject.restartReader();
-		}
 	}
 		
 }
