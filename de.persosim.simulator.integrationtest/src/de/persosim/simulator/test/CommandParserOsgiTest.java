706cbd6186c9ec81b7e66bc281b1d803c27e1fdf
Stefan Philipp <stefan.philipp@secunet.com>
2025-06-16 11:55:11 +0200

    Fix test

--- de.persosim.simulator.integrationtest/src/de/persosim/simulator/test/CommandParserOsgiTest.java
+++ de.persosim.simulator.integrationtest/src/de/persosim/simulator/test/CommandParserOsgiTest.java
@@ -2,6 +2,8 @@ package de.persosim.simulator.test;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.nio.file.Path;
+
 import org.globaltester.PlatformHelper;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,15 +13,18 @@ import org.osgi.framework.FrameworkUtil;
 import de.persosim.simulator.CommandParser;
 import de.persosim.simulator.CommandParserTest;
 import de.persosim.simulator.perso.Personalization;
+import de.persosim.simulator.perso.export.ProfileHelper;
 
 public class CommandParserOsgiTest extends CommandParserTest {
 
-	
+	private static Path pathProfiles = Path.of("../de.persosim.simulator/personalization/profiles");
+
 	@Before
 	public void testInitializer() throws Exception {
 		BundleContext bc = FrameworkUtil.getBundle(this.getClass()).getBundleContext();
-		
+
 		PlatformHelper.startBundle(org.globaltester.cryptoprovider.bc.Activator.class.getPackage().getName(), bc);
+		ProfileHelper.setRootPathPersoFiles(pathProfiles);
 	}
 	/**
 	 * Positive test case: parse personalization from a valid perso identifier within an OSGi-Context
@@ -30,7 +35,7 @@ public class CommandParserOsgiTest extends CommandParserTest {
 		Personalization perso = CommandParser.getPerso("01");
 		assertNotNull(perso);
 	}
-	
+
 	/**
 	 * Negative test case: parse personalization with non-existant perso identifier (too large number
 	 * @throws Exception
